7362be8bf91b9f43582fcd5ccc028d415aa25485
kobrien <kobrien@fdbf22ae-c210-0410-be80-ca943da6b8f8>
1997-11-03 18:57:24 +0000

    Modifications to use only one main file for linking
    git-svn-id: file:///home/users/tmap/svn/repos/ferret/trunk@6214 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- fer/ccr/fermain_c.c
+++ fer/ccr/fermain_c.c
@@ -57,17 +57,99 @@
 *    7/25/97 *js* changes to incorporate output file for -batch
 *    8/97 *kob* - had to add another ifdef check for entry_name_underscores
 *              around call to curv_coord_sub
+*
+*    10/16/97 *kob* - Combining non-gui main program w/ gui main program so that
+*                     there needs only be one main program.  added an ifdef 
+*                     LINK_GUI_AS_MAIN around the gui-exclusive code
 */
 
  
+
 /*#include "tmap_format/ferret.h"*/
+#ifdef LINK_GUI_AS_MAIN
+#  include <X11/Intrinsic.h>
+#  include <X11/StringDefs.h>
+#  include <X11/Xlib.h>
+#endif   /* LINK_GUI_AS_MAIN */
 #include <stdio.h>
 #include <stdlib.h>
+
+
+#ifdef LINK_GUI_AS_MAIN
+/*---------------------------------------------------- 
+ * UxXt.h needs to be included only when compiling a 
+ * stand-alone application. 
+ *---------------------------------------------------*/
+#ifndef DESIGN_TIME
+#include "UxXt.h"
+#endif /* DESIGN_TIME */
+
+XtAppContext	UxAppContext;
+Widget		UxTopLevel;
+Display		*UxDisplay;
+
+int		UxScreen;
+char init_command[128];
+extern void SetInitialState(void);
+
+/*----------------------------------------------
+ * Insert application global declarations here
+ *---------------------------------------------*/
+/*
+ * BEGIN of "FerretMain_mycode_1.c"
+ *
+ * Here are the contents of the file "FerretMain_mycode_1.c"
+ * which was previously included as a separate file.
+ */
+
+#define __globalDefs
+#include "ferret_fortran.h"
+#include "ferret_structures.h"
+#include "ferret_shared_buffer.h"
+#include "JC_Utility.h" /* for windows[] and window_count in the SGI_POPUPS section */
+#undef __globalDefs
+#endif  /* LINK_GUI_AS_MAIN */
+
+#ifndef LINK_GUI_AS_MAIN
 #include "ferret.h"
 #include "ferret_shared_buffer.h"
+#endif  /* LINK_GUI_AS_MAIN */
+
+
+
+#ifdef LINK_GUI_AS_MAIN
+/* .................... Function Definitions .................... */
+
+void help_text(void);
+static void TimeOutCB(XtPointer cd, XtIntervalId *id);
+
+/* .................... Function Declarations .................... */
+
+static void TimeOutCB(XtPointer cd, XtIntervalId *id)
+{
+  XtAppAddTimeOut((XtAppContext)cd, (unsigned long)2000, TimeOutCB, cd);
+  
+#ifdef NO_ENTRY_NAME_UNDERSCORES
+  xgks_x_events();
+#else
+  xgks_x_events_();
+#endif
+
+}
+#endif  /* LINK_GUI_AS_MAIN */
 
 void help_text()
 {
+#ifdef LINK_GUI_AS_MAIN
+  printf(
+"Usage:  ferret [-memsize Mwords] [-batch [outfile]] [-gif] [-gui] [-unmapped] [-help] \n\
+       -memsize:  specify the memory cache size in megawords (default 3.2)\n\
+         -batch:  output directly to metafile \"outfile\" w/out X windows\n\
+      -unmapped:  use invisible output windows (superceded by -batch)\n\
+           -gif:  output to GIF file w/o X windows only w/ FRAME command\n\
+           -gui:  to start Ferret in point and click mode\n\
+          -help:  obtain this listing\n");
+#else
   printf(
 "Usage:  ferret [-memsize Mwords] [-batch [outfile]] [-gif] [-unmapped] [-help] \n\
        -memsize:  specify the memory cache size in megawords (default 3.2)\n\
@@ -75,6 +157,7 @@ void help_text()
       -unmapped:  use invisible output windows (superceded by -batch)\n\
            -gif:  output to GIF file w/o X windows only w/ FRAME command\n\
           -help:  obtain this listing\n");
+#endif  /* LINK_GUI_AS_MAIN */
   exit(0);
 }
 
@@ -88,9 +171,10 @@ main (int argc, char *argv[])
 {
   int status;
   smPtr sBuffer;
-
   char init_command[128];
-  float *memory;
+#ifndef LINK_GUI_AS_MAIN
+  float *memory; 
+#endif
   char *home;
   FILE *fp;
 
@@ -101,13 +185,57 @@ main (int argc, char *argv[])
       mem_size =  PMEM_BLK_SIZE * PMAX_MEM_BLKS,
       ttout_lun=TTOUT_LUN;
   float rmem_size;
+  int using_gui=0;
+#ifdef LINK_GUI_AS_MAIN
+  char tText[128], quote='"';
+  extern void SetInitialState(void);
+
+  /*-----------------------------------------------------------
+   * Declarations.
+   * The default identifier - mainIface will only be declared 
+   * if the interface function is global and of type swidget.
+   * To change the identifier to a different name, modify the
+   * string mainIface in the file "xtmain.dat". If "mainIface"
+   * is declared, it will be used below where the return value
+   * of  PJ_INTERFACE_FUNCTION_CALL will be assigned to it.
+   *----------------------------------------------------------*/ 
+
+  Widget mainIface;
+
+  /*---------------------------------
+   * Interface function declaration
+   *--------------------------------*/	
+
+  Widget  create_FerretMainWd(swidget UxParent);
+  swidget UxParent = NULL;
+
+
+  /*------------------------------------------------------
+   * Declare the fallback resources (added by JC 9.13.96)
+   *-----------------------------------------------------*/
+
+#include "fallback_resources.c"
+
+#endif        /*LINK_GUI_AS_MAIN*/
 
 #ifdef MIXING_NAG_F90_AND_C
   f90_io_init();
 #endif
 
-/* allocate the shared buffer */
-  sBuffer = (sharedMem *)malloc(sizeof(sharedMem));
+
+#ifdef LINK_GUI_AS_MAIN
+/*
+* Make sure the FERRET environment is set up and ready by looking for $FER_DIR.
+*/
+   if (getenv("FER_DIR") == NULL){
+     printf("\n  The FERRET environment has not been properly set up.\n");
+     printf("  (The environment variable FER_DIR is not defined)\n");
+     printf("\n  Have you executed \"source your_system_path/ferret_paths\" ?\n");
+     exit(1);
+   }
+#endif   /* LINK_GUI_AS_MAIN */
+
+
 
 /* decode the command line options: "-memsize", and "-unmapped" */
   while (i<argc) {
@@ -133,6 +261,11 @@ main (int argc, char *argv[])
 #else
       set_batch_graphics_(meta_name);  /* inhibit X output altogether */
 #endif
+#ifdef LINK_GUI_AS_MAIN
+    } else if (strcmp(argv[i],"-gui")==0) {
+      using_gui = 1;  /* new routine added to xopws.c */
+      i++;
+#endif  /* LINK_GUI_AS_MAIN */
     } else  /* -help also comes here */
       help_text();
   }
@@ -149,15 +282,19 @@ main (int argc, char *argv[])
 /* initialize stuff: keyboard, todays date, grids, GFDL terms, PPL brain */
   initialize();
 
-/* turn on ^C interrupts */
+/* turn on ^C interrupts  */
+/* 10/97 *kob* add check for gui now that there is only one main program */
+  if (!using_gui) {
   set_ctrl_c( ctrlc_ast );
-
+}
 /*  prepare appropriate console input state and open the output journal file */
   init_journal( &status );
 
 /* program name and revision number */
+/* 10/97 *kob* add check for gui now that there is only one main program */
+  if (!using_gui ) {
   proclaim_c( &ttout_lun, "\t" );
-
+}
 /* initialize size and shape of memory and linked lists */
   init_memory( &mem_blk_size, &max_mem_blks );
 #else
@@ -165,17 +302,34 @@ main (int argc, char *argv[])
   initialize_();
 
 /* turn on ^C interrupts */
-  set_ctrl_c_( ctrlc_ast_ );
-
+/* 10/97 *kob* add check for gui not that there is only one main program */
+  if (!using_gui ) {
+    set_ctrl_c_( ctrlc_ast_ );
+}
 /*  prepare appropriate console input state and open the output journal file */
   init_journal_( &status );
 
 /* program name and revision number */
-  proclaim_c_( &ttout_lun, "\t" );
-
+/* 10/97 *kob* add check for gui not that there is only one main program */
+  if (!using_gui ) {
+    proclaim_c_( &ttout_lun, "\t" );
+}
 /* initialize size and shape of memory and linked lists */
   init_memory_( &mem_blk_size, &max_mem_blks );
 #endif            /* ENTRY_NAME_UNDERSCORES */
+
+#ifdef LINK_GUI_AS_MAIN
+  /*
+   * ==>> place FERRET under GUI control here  <<== 
+   */
+  if ( using_gui ) {
+#ifdef NO_ENTRY_NAME_UNDERSCORES
+    mode_gui_on();
+#else
+    mode_gui_on_();
+#endif
+  }
+#endif /* LINK_GUI_AS_MAIN */
 
 /* set up to execute $HOME/.ferret if it exists: '\GO "$HOME/.ferret"' */
 /* --> need to see if it exists!!! */
@@ -191,6 +345,13 @@ main (int argc, char *argv[])
     fclose( fp );
   } else
     strcpy( init_command, " " );
+
+  /* GUI OFF -- Only using the command line interface. */
+
+  if ( !using_gui ) {
+
+/* allocate the shared buffer */
+  sBuffer = (sharedMem *)malloc(sizeof(sharedMem));
 
 /* run the initialization file
 * Note 1: do not pass a fixed string as the command - FERRET needs to blank it
@@ -260,6 +421,108 @@ main (int argc, char *argv[])
 
     }
   }
+/******* GUI SECTION *******/
+/* 10/97 *kob* added gui section now that there is only one main program */
+#ifdef LINK_GUI_AS_MAIN
+} else {
+
+
+
+  /*    #include "FerretMain_mycode_3.c"*/
+/*
+ * END of "FerretMain_mycode_3.c"
+ */
+
+#ifdef XOPEN_CATALOG
+    if (XSupportsLocale()) {
+      XtSetLanguageProc(NULL,(XtLanguageProc)NULL,NULL);
+    }
+#endif
+
+
+    /*UxTopLevel = XtAppInitialize(&UxAppContext, "Ferret",
+ 				     NULL, 0, &argc, argv, NULL, NULL, 0);*/
+
+    UxTopLevel = XtVaAppInitialize(&UxAppContext, "Ferret",
+				   NULL, 0, &argc, argv, fallbacks, NULL);
+
+    UxDisplay = XtDisplay(UxTopLevel);
+    UxScreen = XDefaultScreen(UxDisplay);
+
+    /*
+     * We set the geometry of UxTopLevel so that dialogShells
+     * that are parented on it will get centered on the screen
+     * (if defaultPosition is true).
+     */
+
+    XtVaSetValues(UxTopLevel,
+		  XtNx, 0,
+		  XtNy, 0,
+		  XtNwidth, DisplayWidth(UxDisplay, UxScreen),
+		  XtNheight, DisplayHeight(UxDisplay, UxScreen),
+		  NULL);
+
+    /*-------------------------------------------------------
+     * Insert initialization code for your application here 
+     *------------------------------------------------------*/
+	
+    /*----------------------------------------------------------------
+     * Create and popup the first window of the interface.  The 	 
+     * return value can be used in the popdown or destroy functions.
+     * The Widget return value of  PJ_INTERFACE_FUNCTION_CALL will 
+     * be assigned to "mainIface" from  PJ_INTERFACE_RETVAL_TYPE. 
+     *---------------------------------------------------------------*/
+
+    mainIface = create_FerretMainWd(UxParent);
+
+    SetInitialState();
+    UxPopupInterface(mainIface, no_grab);
+
+#ifdef X_REFRESH
+    XtAppAddTimeOut(UxAppContext, 2000, TimeOutCB, UxAppContext);
+#endif
+
+#ifdef SGI_POPUPS
+    /* NB__ UxTopLevel is declared external in UxXt.h */
+    windows[window_count++] = XtWindow(UxTopLevel);
+    XSetWMColormapWindows(XtDisplay(UxTopLevel), XtWindow(UxTopLevel),
+			  windows, window_count);
+#endif /* SGI_POPUPS */
+
+    /*-----------------------
+     * Enter the event loop 
+     *----------------------*/
+    {
+
+      XEvent event;
+
+      for (;;)
+	{
+	  XtAppNextEvent(UxAppContext, &event);
+
+	  if (event.type == ClientMessage) {
+	    ;
+	  }
+	  switch (event.type)
+	    {
+
+	      /*---------------------------------------------------
+	       * Insert code here to handle any events that you do
+	       * not wish to be handled by the interface.
+	       *---------------------------------------------------*/
+	    default:
+	      XtDispatchEvent(&event);
+	      break;
+	    }
+	}
+
+    } /* event loop block */
+
+  } /* END of GUI ON section. */
+#else
+
+}   /* close paren for no_gui section */
+#endif /*LINK_GUI_AS_MAIN*/
 /* 
  *kob* 5/97 - need to close f90 files and flush buffers.....
  */

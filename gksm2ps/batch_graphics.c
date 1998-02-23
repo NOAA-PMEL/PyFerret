34e7dfaa672f959acf80e6000141ad5fd0d021bf
kobrien <kobrien@fdbf22ae-c210-0410-be80-ca943da6b8f8>
1998-02-23 21:44:43 +0000

    new file needed to stub out its_batch_graphics for gksm2ps code
    git-svn-id: file:///home/users/tmap/svn/repos/ferret/trunk@6288 fdbf22ae-c210-0410-be80-ca943da6b8f8

---
+++ gksm2ps/batch_graphics.c
@@ -0,0 +1,42 @@
+/* batch_graphics.c
+
+* contains entries
+*     void set_batch_graphics()    ! sets program state
+* and
+*     int its_batch_graphics       ! queries program state
+
+* programmer - steve hankin
+* NOAA/PMEL, Seattle, WA - Tropical Modeling and Analysis Program
+
+* revision 0.0 - 3/5/97
+
+* compile with
+*    cc -g -c batch_graphics.c
+*  or
+*    cc    -c batch_graphics.c
+
+*/
+
+
+#include <assert.h>
+#include <stdio.h>
+
+#ifdef NO_ENTRY_NAME_UNDERSCORES
+#define FORTRAN(a) a
+#else
+#define FORTRAN(a) a##_
+#endif
+
+/* local static variable to contain the state */
+static int its_batch=0;
+
+/* its_batch_graphics */
+int FORTRAN(its_batch_graphics)()
+{
+   return (its_batch);
+}
+
+
+      
+
+

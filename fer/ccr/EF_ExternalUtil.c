2218dbad0abf29f19570015d089d30cd8875ea8f
callahan <callahan@fdbf22ae-c210-0410-be80-ca943da6b8f8>
1998-05-15 20:38:56 +0000

    fixed a bug in ef_get_bad_flags which wrote beyond the end of the array
    git-svn-id: file:///home/users/tmap/svn/repos/ferret/trunk@6349 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- fer/ccr/EF_ExternalUtil.c
+++ fer/ccr/EF_ExternalUtil.c
@@ -39,7 +39,6 @@ extern float *GLOBAL_bad_flag_ptr;
 /* ... Functions called from the EF code .... */
 
 void ef_set_num_args_( int *, int * );
-void ef_set_num_args( int *, int );
 void ef_set_has_vari_args_( int *, int * );
 void ef_set_axis_inheritance_( int *, int *, int *, int *, int * );
 void ef_set_piecemeal_ok_( int *, int *, int *, int *, int * );
@@ -82,24 +81,13 @@ void ef_set_num_args_(int *id_ptr, int *num_args)
 {
   ExternalFunction *ef_ptr=NULL;
 
+  if ( *num_args > EF_MAX_ARGS ) {
+	fprintf(stderr, "ERROR in ef_set_num_args: %d is greater than maximum: %d\n", *num_args, EF_MAX_ARGS);
+  }
+
   if ( (ef_ptr = ef_ptr_from_id_ptr(id_ptr)) == NULL ) { return; }
 
   ef_ptr->internals_ptr->num_reqd_args = *num_args;
-
-  return;
-}
-
-
-/*
- * Set the number of args for a function. (C version)
- */
-void ef_set_num_args(int *id_ptr, int num_args)
-{
-  ExternalFunction *ef_ptr=NULL;
-
-  if ( (ef_ptr = ef_ptr_from_id_ptr(id_ptr)) == NULL ) { return; }
-
-  ef_ptr->internals_ptr->num_reqd_args = num_args;
 
   return;
 }
@@ -120,14 +108,28 @@ void ef_set_has_vari_args_(int *id_ptr, int *has_vari_args)
 }
 
 
-/*
- * Set the "variable arguments" flag for a function.
- */
 void ef_set_axis_inheritance_(int *id_ptr, int *ax0, int *ax1, int *ax2, int *ax3)
 {
   ExternalFunction *ef_ptr=NULL;
 
   if ( (ef_ptr = ef_ptr_from_id_ptr(id_ptr)) == NULL ) { return; }
+
+  if ( *ax0 != CUSTOM && *ax0 != IMPLIED_BY_ARGS && *ax0 != NORMAL && *ax0 != ABSTRACT ) {
+	  fprintf(stderr, "ERROR in ef_set_axis_inheritance: Axis type not supported on X axis of %s.\n", ef_ptr->name);
+	  fprintf(stderr, "      Please check the spelling of the axis inheritance type.\n");
+	}
+  if ( *ax1 != CUSTOM && *ax1 != IMPLIED_BY_ARGS && *ax1 != NORMAL && *ax1 != ABSTRACT ) {
+	  fprintf(stderr, "ERROR in ef_set_axis_inheritance: Axis type not supported on Y axis of %s.\n", ef_ptr->name);
+	  fprintf(stderr, "      Please check the spelling of the axis inheritance type.\n");
+	}
+  if ( *ax2 != CUSTOM && *ax2 != IMPLIED_BY_ARGS && *ax2 != NORMAL && *ax2 != ABSTRACT ) {
+	  fprintf(stderr, "ERROR in ef_set_axis_inheritance: Axis type not supported on Z axis of %s.\n", ef_ptr->name);
+	  fprintf(stderr, "      Please check the spelling of the axis inheritance type.\n");
+	}
+  if ( *ax3 != CUSTOM && *ax3 != IMPLIED_BY_ARGS && *ax3 != NORMAL && *ax3 != ABSTRACT ) {
+	  fprintf(stderr, "ERROR in ef_set_axis_inheritance: Axis type not supported on T axis of %s.\n", ef_ptr->name);
+	  fprintf(stderr, "      Please check the spelling of the axis inheritance type.\n");
+	}
 
   ef_ptr->internals_ptr->axis_will_be[0] = *ax0;
   ef_ptr->internals_ptr->axis_will_be[1] = *ax1;
@@ -138,9 +140,6 @@ void ef_set_axis_inheritance_(int *id_ptr, int *ax0, int *ax1, int *ax2, int *ax
 }
 
 
-/*
- * Set the "variable arguments" flag for a function.
- */
 void ef_set_piecemeal_ok_(int *id_ptr, int *ax0, int *ax1, int *ax2, int *ax3)
 {
   ExternalFunction *ef_ptr=NULL;
@@ -156,9 +155,6 @@ void ef_set_piecemeal_ok_(int *id_ptr, int *ax0, int *ax1, int *ax2, int *ax3)
 }
 
 
-/*
- * Set the "variable arguments" flag for a function.
- */
 void ef_set_axis_limits_(int *id_ptr, int *axis, int *lo, int *hi)
 {
   ExternalFunction *ef_ptr=NULL;
@@ -172,14 +168,28 @@ void ef_set_axis_limits_(int *id_ptr, int *axis, int *lo, int *hi)
 }
 
 
-/*
- * Set the "variable arguments" flag for a function.
- */
 void ef_set_axis_influence_(int *id_ptr, int *arg, int *ax0, int *ax1, int *ax2, int *ax3)
 {
   ExternalFunction *ef_ptr=NULL;
 
   if ( (ef_ptr = ef_ptr_from_id_ptr(id_ptr)) == NULL ) { return; }
+
+  if ( *ax0 != YES && *ax0 != NO ) {
+	  fprintf(stderr, "ERROR in ef_set_axis_influence on X axis of %s.\n", ef_ptr->name);
+	  fprintf(stderr, "      Please use either YES or NO.\n");
+	}
+  if ( *ax1 != YES && *ax1 != NO ) {
+	  fprintf(stderr, "ERROR in ef_set_axis_influence on Y axis of %s.\n", ef_ptr->name);
+	  fprintf(stderr, "      Please use either YES or NO.\n");
+	}
+  if ( *ax2 != YES && *ax2 != NO ) {
+	  fprintf(stderr, "ERROR in ef_set_axis_influence on Z axis of %s.\n", ef_ptr->name);
+	  fprintf(stderr, "      Please use either YES or NO.\n");
+	}
+  if ( *ax3 != YES && *ax3 != NO ) {
+	  fprintf(stderr, "ERROR in ef_set_axis_influence on T axis of %s.\n", ef_ptr->name);
+	  fprintf(stderr, "      Please use either YES or NO.\n");
+	}
 
   ef_ptr->internals_ptr->axis_implied_from[*arg-1][0] = *ax0;
   ef_ptr->internals_ptr->axis_implied_from[*arg-1][1] = *ax1;
@@ -190,9 +200,6 @@ void ef_set_axis_influence_(int *id_ptr, int *arg, int *ax0, int *ax1, int *ax2,
 }
 
 
-/*
- * Set the "variable arguments" flag for a function.
- */
 void ef_set_axis_extend_(int *id_ptr, int *arg, int *axis, int *lo, int *hi)
 {
   ExternalFunction *ef_ptr=NULL;
@@ -255,7 +262,7 @@ void ef_get_bad_flags_(int *id_ptr, float *bad_flag, float *bad_flag_result)
 {
   int i=0;
 
-  for (i=0; i<EF_MAX_ARGS+1; i++) {
+  for (i=0; i<EF_MAX_ARGS; i++) {
     bad_flag[i] = GLOBAL_bad_flag_ptr[i];
   }
   *bad_flag_result = GLOBAL_bad_flag_ptr[EF_MAX_ARGS];

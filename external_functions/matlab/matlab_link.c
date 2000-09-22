e57feb60d1b997b6f5ef17f546c5dc9e98d63891
ansley <ansley@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2000-09-22 19:35:45 +0000

    initial checkin
    git-svn-id: file:///home/users/tmap/svn/repos/ferret/trunk@7320 fdbf22ae-c210-0410-be80-ca943da6b8f8

---
+++ external_functions/matlab/matlab_link.c
@@ -0,0 +1,84 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "engine.h"
+#define  BUFSIZE 8192
+#define NaN (0.0/0.0)		/* Anyone know a better way? */
+
+static Engine *ep = 0;
+int matlab_demo_(float *data,
+		 float *xcoords, int *xsize,
+		 float *ycoords, int *ysize,
+		 float *bad_flag) {
+  mxArray *T = NULL, *result = NULL;
+  mxArray *mX = NULL, *mY = NULL;
+  char buffer[BUFSIZE], cbuffer[BUFSIZE];
+  double *ddata = 0, *mxData = 0, *myData = 0;;
+
+				/* Open MATLAB engine */
+  if (ep == 0){
+    if (!(ep = engOpen("\0"))) {
+      fprintf(stderr, "\nCan't start MATLAB engine\n");
+      return EXIT_FAILURE;
+    }
+  }
+
+  engOutputBuffer(ep, buffer, BUFSIZE);
+				/* Convert from float->double */
+  T = mxCreateDoubleMatrix(*xsize, *ysize, mxREAL);
+  mxSetName(T, "FERRET_DATA");
+  ddata = mxGetPr(T);
+
+  mX = mxCreateDoubleMatrix(*xsize, 1, mxREAL);
+  mxSetName(mX, "FERRET_XDATA");
+  mxData = mxGetPr(mX);
+  mY = mxCreateDoubleMatrix(*ysize, 1, mxREAL);
+  mxSetName(mY, "FERRET_YDATA");
+  myData = mxGetPr(mY);
+  
+  {
+    int i;
+    for (i=0; i < *xsize * *ysize; ++i){
+      float theData = data[i];
+      if (theData == *bad_flag){
+	ddata[i] = NaN;
+      } else {
+	ddata[i] = data[i];
+      }
+    }
+
+    for (i=0; i < *xsize; ++i){
+      mxData[i] = xcoords[i];
+    }
+    for (i=0; i < *ysize; ++i){
+      myData[i] = ycoords[i];
+    }
+  }
+
+  /* Place the variables into the MATLAB workspace */
+  engPutArray(ep, T);
+  engPutArray(ep, mX);
+  engPutArray(ep, mY);
+
+  
+  sprintf(cbuffer, "ferretdemo(FERRET_DATA', FERRET_XDATA', FERRET_YDATA');");
+  engEvalString(ep, cbuffer);
+  /*
+   * Echo the output from the command.  First two characters are
+   * always the double prompt (>>).
+   */
+  printf("%s", buffer+2);
+
+  mxDestroyArray(T);
+/*   engEvalString(ep, "close;"); */
+/*   engClose(ep); */
+	
+  return EXIT_SUCCESS;
+}
+
+
+
+
+
+
+

e3ce7b006e035377cc0ecf3070da46304b91bbf3
hankin <hankin@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2001-01-02 19:26:11 +0000

    (NEW) added support for string (SHELL COMMANDS) arrays
    git-svn-id: file:///home/users/tmap/svn/repos/ferret/trunk@7375 fdbf22ae-c210-0410-be80-ca943da6b8f8

---
+++ fer/ccr/add_c_pointer.c
@@ -0,0 +1,62 @@
+/*
+*
+*  This software was developed by the Thermal Modeling and Analysis
+*  Project(TMAP) of the National Oceanographic and Atmospheric
+*  Administration's (NOAA) Pacific Marine Environmental Lab(PMEL),
+*  hereafter referred to as NOAA/PMEL/TMAP.
+*
+*  Access and use of this software shall impose the following
+*  obligations and understandings on the user. The user is granted the
+*  right, without any fee or cost, to use, copy, modify, alter, enhance
+*  and distribute this software, and any derivative works thereof, and
+*  its supporting documentation for any purpose whatsoever, provided
+*  that this entire notice appears in all copies of the software,
+*  derivative works and supporting documentation.  Further, the user
+*  agrees to credit NOAA/PMEL/TMAP in any publications that result from
+*  the use of this software or in any product that includes this
+*  software. The names TMAP, NOAA and/or PMEL, however, may not be used
+*  in any advertising or publicity to endorse or promote any products
+*  or commercial entity unless specific written permission is obtained
+*  from NOAA/PMEL/TMAP. The user also understands that NOAA/PMEL/TMAP
+*  is not obligated to provide the user with any support, consulting,
+*  training or assistance of any kind with regard to the use, operation
+*  and performance of this software nor to provide the user with any
+*  updates, revisions, new versions or "bug fixes".
+*
+*  THIS SOFTWARE IS PROVIDED BY NOAA/PMEL/TMAP "AS IS" AND ANY EXPRESS
+*  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+*  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+*  ARE DISCLAIMED. IN NO EVENT SHALL NOAA/PMEL/TMAP BE LIABLE FOR ANY SPECIAL,
+*  INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER
+*  RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
+*  CONTRACT, NEGLIGENCE OR OTHER TORTUOUS ACTION, ARISING OUT OF OR IN
+*  CONNECTION WITH THE ACCESS, USE OR PERFORMANCE OF THIS SOFTWARE.  
+*/
+
+
+/* 
+   Add integer offset to the given pointer.
+   Pointer may be spaced "naturally" (del=0) or at 8 byte intervals (del=1).
+*/
+
+#include <stdlib.h>
+
+double add_c_pointer_(ptr, del, offset)
+     char*** ptr;
+     int* del;
+     int* offset;
+{
+
+  int delta = *del ? 8/sizeof(char**) : 1;
+  union ptr_or_doub
+  {
+    double d;
+    char** ptr;
+  };
+  union ptr_or_doub new_ptr;
+
+  new_ptr.ptr = *ptr + (*offset * delta) ;
+
+  return new_ptr.d;
+
+}

f0e4d9f35217c437436f809f331363942bf2c483
kobrien <kobrien@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2002-01-10 18:58:10 +0000

    removed extra { - weird that it compiled fine under osf
    git-svn-id: file:///home/users/tmap/svn/repos/ferret/trunk@7863 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- fmt/src/ez_delimited_read.c
+++ fmt/src/ez_delimited_read.c
@@ -107,7 +107,6 @@ void FORTRAN(decode_file_jacket)
       
       if (field_type[i] == FTYP_CHARACTER )
 	{
-		{
 	  /* *kob* make sure were using size of real*4 float */
 	  text_fields[i] = (char**) (memptr + ((mr_blk1[mr]-1)*(*mblk_size)*4)/sizeof(float));
 	  mr_c_ptr[mr*pinc] = text_fields[i];

22f79cbddeb5db4dd9b5381c99cfba5be52c3c80
wei <wei@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2004-06-03 20:17:06 +0000

    Added for string match function
    git-svn-id: file:///home/users/tmap/svn/repos/ferret/trunk@8652 fdbf22ae-c210-0410-be80-ca943da6b8f8

---
+++ fmt/src/tm_match_capital_name.c
@@ -0,0 +1,61 @@
+/*
+*
+*  This software was developed by the Thermal Modeling and Analysis
+*  Project(TMAP) of the National Oceanographic and Atmospheric
+*  Administration's (NOAA) Pacific Marine Environmental Lab(PMEL),
+*  hereafter referred to as NOAA/PMEL/TMAP.
+*
+*  Access and use of this software shall impose the following
+*  obligations and understandings on the user. The user is granted the
+*  right, without any fee or cost, to use, copy, modify, alter, enhance
+*  and distribute this software, and any derivative works thereof, and
+*  its supporting documentation for any purpose whatsoever, provided
+*  that this entire notice appears in all copies of the software,
+*  derivative works and supporting documentation.  Further, the user
+*  agrees to credit NOAA/PMEL/TMAP in any publications that result from
+*  the use of this software or in any product that includes this
+*  software. The names TMAP, NOAA and/or PMEL, however, may not be used
+*  in any advertising or publicity to endorse or promote any products
+*  or commercial entity unless specific written permission is obtained
+*  from NOAA/PMEL/TMAP. The user also understands that NOAA/PMEL/TMAP
+*  is not obligated to provide the user with any support, consulting,
+*  training or assistance of any kind with regard to the use, operation
+*  and performance of this software nor to provide the user with any
+*  updates, revisions, new versions or "bug fixes".
+*
+*  THIS SOFTWARE IS PROVIDED BY NOAA/PMEL/TMAP "AS IS" AND ANY EXPRESS
+*  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+*  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+*  ARE DISCLAIMED. IN NO EVENT SHALL NOAA/PMEL/TMAP BE LIABLE FOR ANY SPECIAL,
+*  INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER
+*  RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
+*  CONTRACT, NEGLIGENCE OR OTHER TORTUOUS ACTION, ARISING OUT OF OR IN
+*  CONNECTION WITH THE ACCESS, USE OR PERFORMANCE OF THIS SOFTWARE.  
+*
+*  ywei: 05/04 created to speed up uppercase string matching
+*/
+#include <stdio.h>
+
+void tm_match_captial_name_(char* test_name, 
+                            char* model_name, 
+                            int * len_str, 
+                            int * result)
+{
+     int i, len=*len_str;
+     char c1, c2;
+
+     *result=1;
+     for(i=0; i<len;i++)
+     {
+        c1=test_name[i];
+        c2=model_name[i];
+
+        if(c1!=c2) {
+	  if(c1<'a' || c1>'z' || c1+'A'-'a'!=c2){
+	     *result=0;
+             break;
+	  }
+	}                
+     }
+     return;
+}

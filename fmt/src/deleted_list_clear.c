b2c68779e93e776fa7fc3d74d0ca450ee2746cd1
wei <wei@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2004-07-09 00:25:23 +0000

    added
    git-svn-id: file:///home/users/tmap/svn/repos/ferret/trunk@8756 fdbf22ae-c210-0410-be80-ca943da6b8f8

---
+++ fmt/src/deleted_list_clear.c
@@ -0,0 +1,59 @@
+/*
+*
+*  This software was developed by the Thermal Modeling and Analysis
+*  Project(TMAP) of the National Oceanographic and Atmospheric
+*  Administration's (NOAA) Pacific Marine Environmental Lab(PMEL),
+*  hereafter referred to as NOAA/PMEL/TMAP.
+*
+*  Access and use of this software shall impose the following
+*  obligations and understandings on the user. The user is granted the
+*  right, without any fee or cost, to use, copy, modify, alter, enhance
+*  and distribute this software, and any derivative works thereof, and
+*  its supporting documentation for any purpose whatsoever, provided
+*  that this entire notice appears in all copies of the software,
+*  derivative works and supporting documentation.  Further, the user
+*  agrees to credit NOAA/PMEL/TMAP in any publications that result from
+*  the use of this software or in any product that includes this
+*  software. The names TMAP, NOAA and/or PMEL, however, may not be used
+*  in any advertising or publicity to endorse or promote any products
+*  or commercial entity unless specific written permission is obtained
+*  from NOAA/PMEL/TMAP. The user also understands that NOAA/PMEL/TMAP
+*  is not obligated to provide the user with any support, consulting,
+*  training or assistance of any kind with regard to the use, operation
+*  and performance of this software nor to provide the user with any
+*  updates, revisions, new versions or "bug fixes".
+*
+*  THIS SOFTWARE IS PROVIDED BY NOAA/PMEL/TMAP "AS IS" AND ANY EXPRESS
+*  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+*  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+*  ARE DISCLAIMED. IN NO EVENT SHALL NOAA/PMEL/TMAP BE LIABLE FOR ANY SPECIAL,
+*  INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER
+*  RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
+*  CONTRACT, NEGLIGENCE OR OTHER TORTUOUS ACTION, ARISING OUT OF OR IN
+*  CONNECTION WITH THE ACCESS, USE OR PERFORMANCE OF THIS SOFTWARE.  
+*
+*/
+/*
+   06/04 *ywei* -Created to keep two lists  deleted list and undeleted list)
+                 for better performance. This function is for cleaning up
+                 the data structure.
+ */
+#include <stdio.h>
+#include "deleted_list.h"
+
+void deleted_list_clear_(void * deleted_list_header)
+{
+   int i,j;
+   int array_size;
+   DLHead * head;
+
+   head = (DLHead*)deleted_list_header;
+   array_size = head->array_size;
+   if(head->ptr_table){
+       for(j=1;j<=array_size;j++)
+         free(head->ptr_table[j-1]);
+       free(head->ptr_table);
+   }
+   *((int*)head)=0;
+}
+

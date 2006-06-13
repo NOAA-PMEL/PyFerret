c3d6874ebbfcefe477619a87d0acef9a99d4efe7
ansley <ansley@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2006-06-13 17:51:42 +0000

    new routine fill_xy
    git-svn-id: file:///home/users/tmap/svn/repos/ferret/trunk@9473 fdbf22ae-c210-0410-be80-ca943da6b8f8

---
+++ external_functions/contributed/species.h
@@ -0,0 +1,11 @@
+! source file: /home/andreas/models/UVic/2.6/npzd_co2/ctr/updates/species.h
+c====================== include file "species.h" ==========================
+c
+        integer it_dic
+        real k0,kk1,k2,kkw,kb,ks,kf,k1p,k2p,k3p,ksi
+        real bt,st,ft,sit,pt,dic,ta
+        common /const/k0,kk1,k2,kkw,kb,ks,kf,k1p,k2p,k3p,ksi
+c,ff,htotal
+        common /species/bt,st,ft,sit,pt,dic,ta
+        common /dic/it_dic
+

8d1a6861100856217c53bb00c6e96ba2e92f385e
ansley <ansley@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2006-09-22 16:34:28 +0000

    add gif/ and ps/ includes directories for easier builds
    git-svn-id: file:///home/users/tmap/svn/repos/ferret/trunk@9621 fdbf22ae-c210-0410-be80-ca943da6b8f8

---
+++ xgks/src/lib/gif/gdadds.h
@@ -0,0 +1,9 @@
+#ifndef GIFADDS_H
+#define GIFADDS_h
+void gdImageWideLine(gdImagePtr im, int x1, int y1, int x2, int y2,
+		     int color, int width);
+void gdImageWideLines(gdImagePtr im, gdPointPtr pts, int num,
+		     int color, int width);
+void gdImageBlockFill(gdImagePtr image, int color);
+void gdSetClip(gdImagePtr image, int x1, int y1, int x2, int y2);
+#endif

934b5cd289dd89a07b8e4ee944f33c3039cb4e04
ksmith <ksmith@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2011-03-21 16:32:28 +0000

    Update from ferret trunk
    git-svn-id: file:///home/users/tmap/svn/repos/ferret/branches/pyferret@11891 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- site_specific.mk
+++ site_specific.mk
@@ -2,16 +2,16 @@
 
 # Full path name of the directory containing this file (the ferret root directory).
 # Do not use $(shell pwd) since this is included in Makefiles in other directories.
-DIR_PREFIX	:= $(H)/pyferret_32dev
-# DIR_PREFIX	:= $(H)/pyferret_64dev
+# DIR_PREFIX	:= $(HOME)/pyferret_32dev
+DIR_PREFIX	:= $(HOME)/pyferret_64dev
 
 # Python 2.x executable to invoke for build and install.
-PYTHON_EXE	:= python2.4
-# PYTHON_EXE	:= python2.6
+# PYTHON_EXE	:= python2.4
+PYTHON_EXE	:= python2.6
 
 # Flags for specifying the installation directory for "$(PYTHON_EXE) setup.py install"
-PYTHON_INSTALL_FLAGS	:= --prefix=$(HOME)/.local
-# PYTHON_INSTALL_FLAGS	:= --user
+# PYTHON_INSTALL_FLAGS	:= --prefix=$(HOME)/.local
+PYTHON_INSTALL_FLAGS	:= --user
 
 # Java 1.6 jdk home directory ( $(JAVA_HOME)/bin/javac is called to build threddsBrowser.jar ).
 JAVA_HOME	:= /usr/java/latest

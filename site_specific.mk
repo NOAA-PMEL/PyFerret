85fd8e7e059a639b4f3f184b55e2a130e46df2bb
ksmith <ksmith@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2011-04-13 19:26:05 +0000

    Add customizations for the Ubuntu build; add more info (comments) to ferret7 and site_specific.mk
    git-svn-id: file:///home/users/tmap/svn/repos/ferret/branches/pyferret@12019 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- site_specific.mk
+++ site_specific.mk
@@ -1,29 +1,36 @@
-# Site-dependent definitions included in Makefiles
+## Site-dependent definitions included in Makefiles
 
-# Full path name of the directory containing this file (the ferret root directory).
-# Do not use $(shell pwd) since this is included in Makefiles in other directories.
+## Full path name of the directory containing this file (the ferret root directory).
+## Do not use $(shell pwd) since this is included in Makefiles in other directories.
 # DIR_PREFIX	:= $(HOME)/pyferret_32dev
 DIR_PREFIX	:= $(HOME)/pyferret_64dev
 
-# Python 2.x executable to invoke for build and install.
+## Python 2.x executable to invoke for build and install.
 # PYTHON_EXE	:= python2.4
 PYTHON_EXE	:= python2.6
 
-# Flags for specifying the installation directory for "$(PYTHON_EXE) setup.py install"
+## Flags for specifying the installation directory for "$(PYTHON_EXE) setup.py install"
+## The following is for the user-specific directory $HOME/.local/lib/python2.x/site-packages
 # PYTHON_INSTALL_FLAGS	:= --prefix=$(HOME)/.local
+## The following also specifies the user-specific directory (but not recognized by Python 2.4)
 PYTHON_INSTALL_FLAGS	:= --user
+## The following will install it under $FER_DIR/lib/python2.x/site-packages
 # PYTHON_INSTALL_FLAGS	:= --prefix=$(FER_DIR)
+## The following (empty) will install it in the system-wide package directory
+## (typically /usr/lib/python2.x/site-packages)
+# PYTHON_INSTALL_FLAGS	:=
 
-# Java 1.6 jdk home directory ( $(JAVA_HOME)/bin/javac is called to build threddsBrowser.jar ).
+## Java 1.6 jdk home directory ( $(JAVA_HOME)/bin/javac is called to build threddsBrowser.jar ).
 JAVA_HOME	:= /usr/java/latest
+# JAVA_HOME	:= /usr/lib/jvm/java-1.6.0-sun
 
-# Installation directory for HDF5 (contains include and lib subdirectories)
+## Installation directory for HDF5 (contains include and lib subdirectories)
 HDF5_DIR	:= /usr/local/hdf5_185p1
 
-# Installation directory for NetCDF (contains include and lib subdirectories)
+## Installation directory for NetCDF (contains include and lib subdirectories)
 NETCDF_DIR	:= /usr/local/netcdf_411
 
-# Installation directory for readline v6.x (contains include and lib subdirectories)
+## Installation directory for readline v6.x (contains include and lib subdirectories)
 READLINE_DIR	:= /usr/local/readline_62
 
-#
+##

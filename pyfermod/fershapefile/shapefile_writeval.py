5299ea521361b2a6fe10db91ec8f93800306677a
ksmith <ksmith@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2011-08-11 23:08:01 +0000

    User supplies name of field for value in shapefile; support for Z and T axes removed
    git-svn-id: file:///home/users/tmap/svn/repos/ferret/branches/pyferret@12557 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- pyfermod/fershapefile/shapefile_writeval.py
+++ pyfermod/fershapefile/shapefile_writeval.py
@@ -16,22 +16,26 @@ def ferret_init(efid):
     """
     Initialization for the shapefile_writeval PyEF
     """
-    retdict = { "numargs": 3,
+    retdict = { "numargs": 4,
                 "descript": "Writes a shapefile of XY quadrilaterals that are associated with the given values",
                 "restype": pyferret.FLOAT_ARRAY,
                 "axes": ( pyferret.AXIS_ABSTRACT,
                           pyferret.AXIS_DOES_NOT_EXIST,
                           pyferret.AXIS_DOES_NOT_EXIST,
                           pyferret.AXIS_DOES_NOT_EXIST, ),
-                "argnames": ( "SHAPEFILE", "VALUE", "MAPPRJ"),
+                "argnames": ( "SHAPEFILE", "VALUE", "VALNAME", "MAPPRJ"),
                 "argdescripts": ( "Name for the shapefile (any extension given is ignored)",
-                                  "Value(s) for the shapes; must have X and Y axes, Z and T axes optional",
+                                  "Value(s) for the shapes; must have X and Y axes " \
+                                      "but either no or singleton Z and T axes",
+                                  "Field name for the value in the shapefile",
                                   "Either a common name or a WKT description for the map projection; " \
                                       "if blank, WGS 84 is used", ),
                 "argtypes": ( pyferret.STRING_ONEVAL,
                               pyferret.FLOAT_ARRAY,
+                              pyferret.STRING_ONEVAL,
                               pyferret.STRING_ONEVAL, ),
                 "influences": ( (False, False, False, False),
+                                (False, False, False, False),
                                 (False, False, False, False),
                                 (False, False, False, False), ),
               }
@@ -47,72 +51,64 @@ def ferret_result_limits(efid):
 
 def ferret_compute(efid, result, resbdf, inputs, inpbdfs):
     """
-    Create the shapefile named in inputs[0] using the values array given
-    in inputs[1].  The bounding box limits of the X and Y axes of inputs[1]
-    are used to create the quadrilaterals.  If given, Z axis values will
-    be included in the shapes coordinates and generate layers of shapes.
-    If given, T axis values will generate field names in the database of
-    values associated with each shape.  The values in inputs[1] is then
-    used as the values associated with each shape.  Either a common name
-    or a WKT description of the map projection for the coordinated should
-    be given in inputs[2].  If blank, WGS 84 is used.  If successful,
-    fills result (which might as well be a 1x1x1x1 array) with zeros.
-    If problems, an error will be raised.
+    Create the shapefile named in inputs[0] using the values array
+    given in inputs[1].  The bounding box limits of the X and Y axes
+    of inputs[1] are used to create the quadrilaterals.  The values
+    in inputs[1] are used as the values associated with each shape
+    using the field name given in inputs[2].  Either a common name
+    or a WKT description of the map projection for the coordinated
+    should be given in inputs[3].  If blank, WGS 84 is used.  If
+    successful, fills result (which might as well be a 1x1x1x1 array)
+    with zeros.  If problems, an error will be raised.
     """
     shapefile_name = inputs[0]
-    shapefile_values = inputs[1]
-    shapefile_mapprj = inputs[2]
-    # Get the X and Y axis box limits for the quadrilateral coordinates
+    values = inputs[1]
+    missing_value = inpbdfs[1]
+    field_name = inputs[2].strip()
+    if not field_name:
+        field_name = "Value"
+    map_projection = inputs[3]
+
+    # Verify the shapes are as expected
+    if (values.shape[2] > 1) or (values.shape[3] > 1):
+        raise ValueError("The Z and T axes of the given values must be undefined or singleton axes")
+
+    # Get the X axis box limits for the quadrilateral coordinates
     x_box_limits = pyferret.get_axis_box_limits(efid, pyferret.ARG2, pyferret.X_AXIS)
     if x_box_limits == None:
         raise ValueError("Unable to determine the X axis box limits")
     lowerxs = x_box_limits[0]
     upperxs = x_box_limits[1]
+
+    # Get the Y axis box limits for the quadrilateral coordinates
     y_box_limits = pyferret.get_axis_box_limits(efid, pyferret.ARG2, pyferret.Y_AXIS)
     if y_box_limits == None:
         raise ValueError("Unable to determine the y axis box limits")
     lowerys = y_box_limits[0]
     upperys = y_box_limits[1]
-    # Get the elevation/depth coordinates, or None if they do not exist
-    z_coords = pyferret.get_axis_coordinates(efid, pyferret.ARG2, pyferret.Z_AXIS)
-    # Create the shapefile object with the appropriate shape type
-    if z_coords == None:
-        sfwriter = shapefile.Writer(shapefile.POLYGON)
-    else:
-        sfwriter = shapefile.Writer(shapefile.POLYGONZ)
-    # Time coordinates, if they exist, are used for field names of values associated with each shape
-    # TODO: need get_time_axis_coordinates in order to generate meaningful values
-    t_coords = pyferret.get_axis_coordinates(efid, pyferret.ARG2, pyferret.T_AXIS)
-    # Create the field(s) for the values
-    # TODO: get reasonable names and sizes for the fields
-    if t_coords == None:
-        sfwriter.field("Value", "N", 20, 7)
-    else:
-        for t in t_coords:
-            sfwriter.field("T%d" % t, "N", 20, 7)
+
+    # Create polygons with a single field value
+    sfwriter = shapefile.Writer(shapefile.POLYGON)
+    sfwriter.field(field_name, "N", 20, 7)
+
     # Write out the shapes and the records
-    if z_coords == None:
-        for j in xrange(len(lowerys)):
-            for i in xrange(len(lowerxs)):
+    shape_written = False
+    for j in xrange(len(lowerys)):
+        for i in xrange(len(lowerxs)):
+            if values[i, j, 0, 0] != missing_value:
+                shape_written = True
                 pyferret.fershapefile.addquadxyvalues(sfwriter,
-                         ( lowerxs[i], lowerys[j] ),
-                         ( lowerxs[i], upperys[j] ),
-                         ( upperxs[i], upperys[j] ),
-                         ( upperxs[i], lowerys[j] ),
-                         None, shapefile_values[i, j, 0, :].tolist())
-    else:
-        for k in xrange(len(z_coords)):
-            for j in xrange(len(lowerys)):
-                for i in xrange(len(lowerxs)):
-                    pyferret.fershapefile.addquadxyvalues(sfwriter,
-                             ( lowerxs[i], lowerys[j] ),
-                             ( lowerxs[i], upperys[j] ),
-                             ( upperxs[i], upperys[j] ),
-                             ( upperxs[i], lowerys[j] ),
-                             z_coords[k], shapefile_values[i, j, k, :].tolist())
+                                      ( lowerxs[i], lowerys[j] ),
+                                      ( lowerxs[i], upperys[j] ),
+                                      ( upperxs[i], upperys[j] ),
+                                      ( upperxs[i], lowerys[j] ),
+                                      None, [ float(values[i, j, 0, 0]) ])
+    if not shape_written:
+        raise ValueError("All values are missing values")
     sfwriter.save(shapefile_name)
+
     # Create the .prj file from the map projection common name or the WKT description
-    pyferret.fershapefile.createprjfile(shapefile_mapprj, shapefile_name)
+    pyferret.fershapefile.createprjfile(map_projection, shapefile_name)
     result[:, :, :, :] = 0
 
 
--- pyfermod/fershapefile/shapefile_writexyval.py
+++ pyfermod/fershapefile/shapefile_writexyval.py
@@ -2,7 +2,8 @@
 Creates a shapefile with a given root name using data from given X, Y,
 and value arrays (curvilinear-type data).  The shapes are quadrilaterals
 in the X,Y-plane derived from the X and Y arrays.  The value associated
-with each shape comes from the value array.
+with each quadrilateral comes from the value array.  Quadrilaterals
+associated with missing values are omitted from the shapefile.
 """
 
 import shapefile
@@ -13,26 +14,29 @@ def ferret_init(efid):
     """
     Initialization for the shapefile_writexyval PyEF
     """
-    retdict = { "numargs": 5,
+    retdict = { "numargs": 6,
                 "descript": "Writes a shapefile of XY quadrilaterals from the curvilinear data arrays.",
                 "restype": pyferret.FLOAT_ARRAY,
                 "axes": ( pyferret.AXIS_ABSTRACT,
                           pyferret.AXIS_DOES_NOT_EXIST,
                           pyferret.AXIS_DOES_NOT_EXIST,
                           pyferret.AXIS_DOES_NOT_EXIST, ),
-                "argnames": ( "SHAPEFILE", "GRIDX", "GRIDY", "GRIDVALS", "MAPPRJ"),
+                "argnames": ( "SHAPEFILE", "GRIDX", "GRIDY", "GRIDVALS", "VALNAME", "MAPPRJ"),
                 "argdescripts": ( "Name for the shapefile (any extension given is ignored)",
                                   "X (Longitude) grid values for the shapefile; must be 2D on X and Y",
                                   "Y (Latitude) grid values for the shapefile; must be 2D on X and Y",
                                   "Values for the shapes (or grid centers); must be 2D on X and Y",
+                                  "Field name for the value in the shapefile",
                                   "Either a common name or a WKT description for the map projection; " \
                                       "if blank, WGS 84 is used", ),
                 "argtypes": ( pyferret.STRING_ONEVAL,
                               pyferret.FLOAT_ARRAY,
                               pyferret.FLOAT_ARRAY,
                               pyferret.FLOAT_ARRAY,
+                              pyferret.STRING_ONEVAL,
                               pyferret.STRING_ONEVAL, ),
                 "influences": ( (False, False, False, False),
+                                (False, False, False, False),
                                 (False, False, False, False),
                                 (False, False, False, False),
                                 (False, False, False, False),
@@ -54,40 +58,53 @@ def ferret_compute(efid, result, resbdf, inputs, inpbdfs):
     in inputs[1], grid Y coordinates given in inputs[2], and grid values given
     in inputs[3].  The X,Y coordinates are used for the quadrilaterals vertices
     and must have an additional value along each dimension.  The value [i,j]
-    is used for the quadrilateral with diagonal corners [i, j] and [i+1, j+1]. 
-    Either a common name or a WKT description of the map projection for the
-    coordinated should be given in inputs[4].  If blank, WGS 84 is used. 
-    If successful, fills result (which might as well be a 1x1x1x1 array) with
-    zeros.  If problems, an error will be raised.
+    is used for the quadrilateral with diagonal corners [i, j] and [i+1, j+1].
+    Quadrilateral associated with missing values are omitted from the shapefile.
+    The field name for the value in the shapefile given in inputs[4].  Either a
+    common name or a WKT description of the map projection for the coordinates
+    should be given in inputs[5].  If blank, WGS 84 is used.  If successful,
+    fills result (which might as well be a 1x1x1x1 array) with zeros.  An error
+    will be raised if problem occur.
     """
     shapefile_name = inputs[0]
     grid_xs = inputs[1]
     grid_ys = inputs[2]
     grid_vals = inputs[3]
-    shapefile_mapprj = inputs[4]
+    missing_val = inpbdfs[3]
+    field_name = inputs[4].strip()
+    if not field_name:
+        field_name = "Value"
+    map_projection = inputs[5]
+
     # Verify the shapes are as expected
     if (grid_vals.shape[2] != 1) or (grid_vals.shape[3] != 1):
-        raise ValueError("GRIDVALS Z and T axes must be undefined or singleton axes")
+        raise ValueError("The Z and T axes of GRIDVALS must be undefined or singleton axes")
     exp_shape = ( grid_vals.shape[0] + 1, grid_vals.shape[1] + 1, 1, 1 )
     if (grid_xs.shape != exp_shape) or (grid_ys.shape != exp_shape):
-         raise ValueError('GRIDX and GRIDY must have one additional element along the X and Y axes')
-    # Create the shapefile writer object
+         raise ValueError('GRIDX and GRIDY must have one additional element along the X and Y axes compared to GRIDVALS')
+
+    # Create polygons with a single field value
     sfwriter = shapefile.Writer(shapefile.POLYGON)
-    # Create the field for the value
-    # TODO: get reasonable name and sizes for the field
-    sfwriter.field("Value", "N", 20, 7)
-    # Add all the shapes with their values
+    sfwriter.field(field_name, "N", 20, 7)
+
+    # Add the shapes with their values
+    shape_written = False
     for j in xrange(grid_vals.shape[1]):
         for i in xrange(grid_vals.shape[0]):
-            pyferret.fershapefile.addquadxyvalues(sfwriter,
-                     (grid_xs[i,   j,   0, 0], grid_ys[i,   j,   0, 0]),
-                     (grid_xs[i,   j+1, 0, 0], grid_ys[i,   j+1, 0, 0]),
-                     (grid_xs[i+1, j+1, 0, 0], grid_ys[i+1, j+1, 0, 0]),
-                     (grid_xs[i+1, j,   0, 0], grid_ys[i+1, j,   0, 0]),
-                     None, grid_vals[i, j, 0, :].tolist())
+            if grid_vals[i, j, 0, 0] != missing_val:
+                shape_written = True
+                pyferret.fershapefile.addquadxyvalues(sfwriter,
+                         (grid_xs[i,   j,   0, 0], grid_ys[i,   j,   0, 0]),
+                         (grid_xs[i,   j+1, 0, 0], grid_ys[i,   j+1, 0, 0]),
+                         (grid_xs[i+1, j+1, 0, 0], grid_ys[i+1, j+1, 0, 0]),
+                         (grid_xs[i+1, j,   0, 0], grid_ys[i+1, j,   0, 0]),
+                         None, [ float(grid_vals[i, j, 0, 0]) ])
+    if not shape_written:
+        raise ValueError("All values are missing values")
     sfwriter.save(shapefile_name)
+
     # Create the .prj file from the map projection common name or the WKT description
-    pyferret.fershapefile.createprjfile(shapefile_mapprj, shapefile_name)
+    pyferret.fershapefile.createprjfile(map_projection, shapefile_name)
     result[:, :, :, :] = 0
 
 
@@ -98,38 +115,86 @@ if __name__ == "__main__":
     import numpy
     import os
 
-    shapefilename = "testsf"
+    shapefilename = "tripolar"
+    fieldname = "area"
     wgs84_descript = 'GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563]],PRIMEM["Greenwich",0],UNIT["degree",0.0174532925199433]]'
 
-    # values from tripolar coordinates X=90E:120W:10,Y=45N:85N:5
+    # real world longitudes and latitudes of tripolar coordinates X=80W:60E:10 + 100E:120W:10,Y=45N:85N:5
     geolon_c = numpy.array([
-     [ 83.8,  87.9,  92.3,  97.6, 104.3, 113.3, 126.2, 145.1, 170.0, 194.9, 213.8, 226.7, 235.7, 242.4, 247.7, 252.1 ],
-     [ 86.9,  94.0, 101.5, 109.8, 119.1, 129.8, 141.9, 155.5, 170.0, 184.5, 198.1, 210.2, 220.9, 230.2, 238.5, 246.0 ],
-     [ 88.8,  97.7, 106.9, 116.4, 126.3, 136.7, 147.5, 158.6, 170.0, 181.4, 192.5, 203.3, 213.7, 223.6, 233.1, 242.3 ],
-     [ 89.7,  99.5, 109.4, 119.3, 129.3, 139.4, 149.5, 159.7, 170.0, 180.3, 190.5, 200.6, 210.7, 220.7, 230.6, 240.5 ],
-     [ 90.0, 100.0, 110.0, 120.0, 130.0, 140.0, 150.0, 160.0, 170.0, 180.0, 190.0, 200.0, 210.0, 220.0, 230.0, 240.0 ],
-     [ 90.0, 100.0, 110.0, 120.0, 130.0, 140.0, 150.0, 160.0, 170.0, 180.0, 190.0, 200.0, 210.0, 220.0, 230.0, 240.0 ],
-     [ 90.0, 100.0, 110.0, 120.0, 130.0, 140.0, 150.0, 160.0, 170.0, 180.0, 190.0, 200.0, 210.0, 220.0, 230.0, 240.0 ],
-     [ 90.0, 100.0, 110.0, 120.0, 130.0, 140.0, 150.0, 160.0, 170.0, 180.0, 190.0, 200.0, 210.0, 220.0, 230.0, 240.0 ],
-     [ 90.0, 100.0, 110.0, 120.0, 130.0, 140.0, 150.0, 160.0, 170.0, 180.0, 190.0, 200.0, 210.0, 220.0, 230.0, 240.0 ],
+        [  -92.1, -87.7, -82.4, -75.7, -66.7, -53.8, -34.9, -10.0,  14.9,  33.8,  
+            46.7,  55.7,  62.4,  67.7,  72.1,  87.9,  92.3,  97.6, 104.3, 113.3, 
+           126.2, 145.1, 170.0, 194.9, 213.8, 226.7, 235.7, 242.4, 247.7, 252.1, 267.9, ],
+        [  -86.0, -78.5, -70.2, -60.9, -50.2, -38.1, -24.5, -10.0,   4.5,  18.1,  
+            30.2,  40.9,  50.2,  58.5,  66.0,  94.0, 101.5, 109.8, 119.1, 129.8, 
+           141.9, 155.5, 170.0, 184.5, 198.1, 210.2, 220.9, 230.2, 238.5, 246.0, 274.0, ],
+        [  -82.3, -73.1, -63.6, -53.7, -43.3, -32.5, -21.4, -10.0,   1.4,  12.5,  
+            23.3,  33.7,  43.6,  53.1,  62.3,  97.7, 106.9, 116.4, 126.3, 136.7, 
+           147.5, 158.6, 170.0, 181.4, 192.5, 203.3, 213.7, 223.6, 233.1, 242.3, 277.7, ],
+        [  -80.5, -70.6, -60.7, -50.7, -40.6, -30.5, -20.3, -10.0,   0.3,  10.5,  
+            20.6,  30.7,  40.7,  50.6,  60.5,  99.5, 109.4, 119.3, 129.3, 139.4, 
+           149.5, 159.7, 170.0, 180.3, 190.5, 200.6, 210.7, 220.7, 230.6, 240.5, 279.5, ],
+        [  -80.0, -70.0, -60.0, -50.0, -40.0, -30.0, -20.0, -10.0,   0.0,  10.0,  
+            20.0,  30.0,  40.0,  50.0,  60.0, 100.0, 110.0, 120.0, 130.0, 140.0, 
+           150.0, 160.0, 170.0, 180.0, 190.0, 200.0, 210.0, 220.0, 230.0, 240.0, 280.0, ],
+        [  -80.0, -70.0, -60.0, -50.0, -40.0, -30.0, -20.0, -10.0,   0.0,  10.0,  
+            20.0,  30.0,  40.0,  50.0,  60.0, 100.0, 110.0, 120.0, 130.0, 140.0, 
+           150.0, 160.0, 170.0, 180.0, 190.0, 200.0, 210.0, 220.0, 230.0, 240.0, 280.0, ],
+        [  -80.0, -70.0, -60.0, -50.0, -40.0, -30.0, -20.0, -10.0,   0.0,  10.0,  
+            20.0,  30.0,  40.0,  50.0,  60.0,  100.0, 110.0, 120.0, 130.0, 140.0, 
+           150.0, 160.0, 170.0, 180.0, 190.0, 200.0, 210.0, 220.0, 230.0, 240.0, 280.0, ],
+        [  -80.0, -70.0, -60.0, -50.0, -40.0, -30.0, -20.0, -10.0,   0.0,  10.0,  
+            20.0,  30.0,  40.0,  50.0,  60.0, 100.0, 110.0, 120.0, 130.0, 140.0, 
+           150.0, 160.0, 170.0, 180.0, 190.0, 200.0, 210.0, 220.0, 230.0, 240.0, 280.0, ],
+        [  -80.0, -70.0, -60.0, -50.0, -40.0, -30.0, -20.0, -10.0,   0.0,  10.0,  
+            20.0,  30.0,  40.0,  50.0,  60.0, 100.0, 110.0, 120.0, 130.0, 140.0, 
+           150.0, 160.0, 170.0, 180.0, 190.0, 200.0, 210.0, 220.0, 230.0, 240.0, 280.0, ], 
     ], dtype=numpy.float32)
     geolon_c = geolon_c.T[:, :, numpy.newaxis, numpy.newaxis]
     geolat_c = numpy.array([
-     [ 68.65, 71.85, 74.69, 77.25, 79.54, 81.58, 83.30, 84.53, 85.00, 84.53, 83.30, 81.58, 79.54, 77.25, 74.69, 71.85 ],
-     [ 67.92, 70.51, 72.81, 74.83, 76.56, 77.99, 79.08, 79.76, 80.00, 79.76, 79.08, 77.99, 76.56, 74.83, 72.81, 70.51 ],
-     [ 66.94, 68.71, 70.29, 71.67, 72.83, 73.76, 74.44, 74.86, 75.00, 74.86, 74.44, 73.76, 72.83, 71.67, 70.29, 68.71 ],
-     [ 65.93, 66.80, 67.60, 68.30, 68.90, 69.37, 69.72, 69.93, 70.00, 69.93, 69.72, 69.37, 68.90, 68.30, 67.60, 66.80 ],
-     [ 65.00, 65.00, 65.00, 65.00, 65.00, 65.00, 65.00, 65.00, 65.00, 65.00, 65.00, 65.00, 65.00, 65.00, 65.00, 65.00 ],
-     [ 60.00, 60.00, 60.00, 60.00, 60.00, 60.00, 60.00, 60.00, 60.00, 60.00, 60.00, 60.00, 60.00, 60.00, 60.00, 60.00 ],
-     [ 55.00, 55.00, 55.00, 55.00, 55.00, 55.00, 55.00, 55.00, 55.00, 55.00, 55.00, 55.00, 55.00, 55.00, 55.00, 55.00 ],
-     [ 50.00, 50.00, 50.00, 50.00, 50.00, 50.00, 50.00, 50.00, 50.00, 50.00, 50.00, 50.00, 50.00, 50.00, 50.00, 50.00 ],
-     [ 45.00, 45.00, 45.00, 45.00, 45.00, 45.00, 45.00, 45.00, 45.00, 45.00, 45.00, 45.00, 45.00, 45.00, 45.00, 45.00 ],
+        [ 71.85, 74.69, 77.25, 79.54, 81.58, 83.30, 84.53, 85.00, 84.53, 83.30, 
+          81.58, 79.54, 77.25, 74.69, 71.85, 71.85, 74.69, 77.25, 79.54, 81.58, 
+          83.30, 84.53, 85.00, 84.53, 83.30, 81.58, 79.54, 77.25, 74.69, 71.85, 71.85, ],
+        [ 70.51, 72.81, 74.83, 76.56, 77.99, 79.08, 79.76, 80.00, 79.76, 79.08, 
+          77.99, 76.56, 74.83, 72.81, 70.51, 70.51, 72.81, 74.83, 76.56, 77.99, 
+          79.08, 79.76, 80.00, 79.76, 79.08, 77.99, 76.56, 74.83, 72.81, 70.51, 70.51, ],
+        [ 68.71, 70.29, 71.67, 72.83, 73.76, 74.44, 74.86, 75.00, 74.86, 74.44, 
+          73.76, 72.83, 71.67, 70.29, 68.71, 68.71, 70.29, 71.67, 72.83, 73.76, 
+          74.44, 74.86, 75.00, 74.86, 74.44, 73.76, 72.83, 71.67, 70.29, 68.71, 68.71, ],
+        [ 66.80, 67.60, 68.30, 68.90, 69.37, 69.72, 69.93, 70.00, 69.93, 69.72, 
+          69.37, 68.90, 68.30, 67.60, 66.80, 66.80, 67.60, 68.30, 68.90, 69.37, 
+          69.72, 69.93, 70.00, 69.93, 69.72, 69.37, 68.90, 68.30, 67.60, 66.80, 66.80, ],
+        [ 65.00, 65.00, 65.00, 65.00, 65.00, 65.00, 65.00, 65.00, 65.00, 65.00, 
+          65.00, 65.00, 65.00, 65.00, 65.00, 65.00, 65.00, 65.00, 65.00, 65.00, 
+          65.00, 65.00, 65.00, 65.00, 65.00, 65.00, 65.00, 65.00, 65.00, 65.00, 65.00, ],
+        [ 60.00, 60.00, 60.00, 60.00, 60.00, 60.00, 60.00, 60.00, 60.00, 60.00, 
+          60.00, 60.00, 60.00, 60.00, 60.00, 60.00, 60.00, 60.00, 60.00, 60.00, 
+          60.00, 60.00, 60.00, 60.00, 60.00, 60.00, 60.00, 60.00, 60.00, 60.00, 60.00, ],
+        [ 55.00, 55.00, 55.00, 55.00, 55.00, 55.00, 55.00, 55.00, 55.00, 55.00, 
+          55.00, 55.00, 55.00, 55.00, 55.00, 55.00, 55.00, 55.00, 55.00, 55.00, 
+          55.00, 55.00, 55.00, 55.00, 55.00, 55.00, 55.00, 55.00, 55.00, 55.00, 55.00, ],
+        [ 50.00, 50.00, 50.00, 50.00, 50.00, 50.00, 50.00, 50.00, 50.00, 50.00, 
+          50.00, 50.00, 50.00, 50.00, 50.00, 50.00, 50.00, 50.00, 50.00, 50.00, 
+          50.00, 50.00, 50.00, 50.00, 50.00, 50.00, 50.00, 50.00, 50.00, 50.00, 50.00, ],
+        [ 45.00, 45.00, 45.00, 45.00, 45.00, 45.00, 45.00, 45.00, 45.00, 45.00, 
+          45.00, 45.00, 45.00, 45.00, 45.00, 45.00, 45.00, 45.00, 45.00, 45.00, 
+          45.00, 45.00, 45.00, 45.00, 45.00, 45.00, 45.00, 45.00, 45.00, 45.00, 45.00, ],
     ], dtype=numpy.float32)
     geolat_c = geolat_c.T[:, :, numpy.newaxis, numpy.newaxis]
-    vals  = (geolon_c[:-1, :-1, :, :] * geolat_c[1:,  :-1, :, :] - geolon_c[1:,  :-1, :, :]) * geolat_c[:-1, :-1, :, :]
-    vals += (geolon_c[1:,  :-1, :, :] * geolat_c[1:,  1:,  :, :] - geolon_c[1:,  1:,  :, :]) * geolat_c[1:,  :-1, :, :]
-    vals += (geolon_c[1:,  1:,  :, :] * geolat_c[:-1, 1:,  :, :] - geolon_c[:-1, 1:,  :, :]) * geolat_c[1:,  1:,  :, :]
-    vals += (geolon_c[:-1, 1:,  :, :] * geolat_c[:-1, :-1, :, :] - geolon_c[:-1, :-1, :, :]) * geolat_c[:-1, 1:,  :, :]
+
+    # Make the value an approximate sphere surface area (in square degrees) of the quadrilateral
+    vals  = geolon_c[:-1, :-1] * geolat_c[:-1,  1:] 
+    vals -= geolon_c[:-1,  1:] * geolat_c[:-1, :-1]
+    vals += geolon_c[:-1,  1:] * geolat_c[ 1:,  1:]
+    vals -= geolon_c[ 1:,  1:] * geolat_c[:-1,  1:]
+    vals += geolon_c[ 1:,  1:] * geolat_c[ 1:, :-1]
+    vals -= geolon_c[ 1:, :-1] * geolat_c[ 1:,  1:]
+    vals += geolon_c[ 1:, :-1] * geolat_c[:-1, :-1]
+    vals -= geolon_c[:-1, :-1] * geolat_c[ 1:, :-1]
+    vals = 0.5 * numpy.fabs(vals)
+    vals *= numpy.cos( 0.25 * numpy.deg2rad(geolat_c[:-1, :-1] + \
+                                            geolat_c[:-1,  1:] + \
+                                            geolat_c[ 1:,  1:] + \
+                                            geolat_c[ 1:, :-1]) )
 
     # make sure these calls do not generate errors
     info = ferret_init(0)
@@ -137,10 +202,10 @@ if __name__ == "__main__":
     limits = ferret_result_limits(0)
     del limits
 
-    resbdf = numpy.array([9999.0], dtype=numpy.float32)
-    inpbdfs = numpy.array([8888.0, 7777.0, 6666.0, 5555.0, 4444.0, 3333.0], dtype=numpy.float32)
+    resbdf = numpy.array([-99999.0], dtype=numpy.float32)
+    inpbdfs = numpy.array([-88888.0, -77777.0, -66666.0, -55555.0, -44444.0, -33333.0], dtype=numpy.float32)
     result = numpy.ones((1,1,1,1), dtype=numpy.float32)
-    ferret_compute(0, result, resbdf, (shapefilename, geolon_c, geolat_c, vals, ""), inpbdfs)
+    ferret_compute(0, result, resbdf, (shapefilename, geolon_c, geolat_c, vals, fieldname, ""), inpbdfs)
 
     sfreader = shapefile.Reader(shapefilename)
     shapes = sfreader.shapes()
@@ -182,10 +247,52 @@ if __name__ == "__main__":
                          "    expect: %s\n" \
                          "    found:  %s" % (wgs84_descript, descript))
 
-    # os.remove("%s.dbf" % shapefilename)
-    # os.remove("%s.shp" % shapefilename)
-    # os.remove("%s.shx" % shapefilename)
-    # os.remove("%s.prj" % shapefilename)
+    os.remove("%s.dbf" % shapefilename)
+    os.remove("%s.shp" % shapefilename)
+    os.remove("%s.shx" % shapefilename)
+    os.remove("%s.prj" % shapefilename)
+
+    testcode = """
+    sortedvals = numpy.sort(vals.reshape(-1))
+    numvals = sortedvals.shape[0]
+    limits = [ sortedvals[1 * numvals // 5],
+               sortedvals[2 * numvals // 5],
+               sortedvals[3 * numvals // 5],
+               sortedvals[4 * numvals // 5], ]
+    print str( [ sortedvals[0] ] + limits + [ sortedvals[-1] ] )
+
+    partvals = vals.copy()
+    partvals[ partvals >= limits[0] ] = inpbdfs[3]
+    ferret_compute(0, result, resbdf, (shapefilename + "_1",
+                                       geolon_c, geolat_c, partvals,
+                                       fieldname, ""), inpbdfs)
+
+    partvals = vals.copy()
+    partvals[ partvals <  limits[0] ] = inpbdfs[3]
+    partvals[ partvals >= limits[1] ] = inpbdfs[3]
+    ferret_compute(0, result, resbdf, (shapefilename + "_2",
+                                       geolon_c, geolat_c, partvals,
+                                       fieldname, ""), inpbdfs)
+
+    partvals = vals.copy()
+    partvals[ partvals <  limits[1] ] = inpbdfs[3]
+    partvals[ partvals >= limits[2] ] = inpbdfs[3]
+    ferret_compute(0, result, resbdf, (shapefilename + "_3",
+                                       geolon_c, geolat_c, partvals,
+                                       fieldname, ""), inpbdfs)
+    partvals = vals.copy()
+    partvals[ partvals <  limits[2] ] = inpbdfs[3]
+    partvals[ partvals >= limits[3] ] = inpbdfs[3]
+    ferret_compute(0, result, resbdf, (shapefilename + "_4",
+                                       geolon_c, geolat_c, partvals,
+                                       fieldname, ""), inpbdfs)
+
+    partvals = vals.copy()
+    partvals[ partvals <  limits[3] ] = inpbdfs[3]
+    ferret_compute(0, result, resbdf, (shapefilename + "_5",
+                                       geolon_c, geolat_c, partvals,
+                                       fieldname, ""), inpbdfs)
+    """
 
     print "shapefile_writexyval: SUCCESS"
 

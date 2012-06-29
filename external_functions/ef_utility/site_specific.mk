6593c46626b38338da2b7509b5b2d45ae55d3958
ansley <ansley@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2012-06-29 21:31:51 +0000

    merged ferret6d branch with the trunk
    git-svn-id: file:///home/users/tmap/svn/repos/ferret/trunk@14259 fdbf22ae-c210-0410-be80-ca943da6b8f8

---
+++ external_functions/ef_utility/site_specific.mk
@@ -0,0 +1,11 @@
+## Site-dependent definitions included in Makefiles
+
+## Machine type for which to build the local external functions
+## Use $(HOSTTYPE) to build natively for the machine you are using
+BUILDTYPE	= $(HOSTTYPE)
+# BUILDTYPE	= x86_64-linux
+# BUILDTYPE	= i386-linux
+
+## Installation directory for the locally built external functions
+FER_LOCAL_EXTFCNS = $(FER_DIR)/ext_func/libs
+

4e12aae0853a8b73d4585246562093784421def4
ansley <ansley@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2012-08-06 20:47:02 +0000

    need def of ssize_t for the Mac
    git-svn-id: file:///home/users/tmap/svn/repos/ferret/trunk@14600 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- fmt/src/deleted_list_init.c
+++ fmt/src/deleted_list_init.c
@@ -39,6 +39,11 @@
                  data structure.
  */
 /* *acm   9/06 v600 - add stdlib.h wherever there is stdio.h for altix build*/ 
+
+#ifdef MAC_SSIZE
+typedef long ssize_t;
+#endif
+
 #include <stddef.h>  /* size_t, ptrdiff_t; gfortran on linux rh5*/
 # include <stdio.h>
 #include <stdlib.h>

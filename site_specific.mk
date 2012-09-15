ed04e45fe50e817eaafe10a86c2deb28b3791647
ksmith <ksmith@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2012-09-15 00:35:56 +0000

    Merge from ferret trunk - use hdf5-1.8.9 and netcdf-4.2.1.1
    git-svn-id: file:///home/users/tmap/svn/repos/ferret/branches/pyferret@14859 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- site_specific.mk
+++ site_specific.mk
@@ -26,13 +26,13 @@ INSTALL_FER_DIR	= $(FER_DIR)
 ## (contains include and lib or lib64 subdirectories)
 # HDF5_DIR	= /usr
 # HDF5_DIR	= /usr/local
-HDF5_DIR	= /usr/local/hdf5_188
+HDF5_DIR	= /usr/local/hdf5_189
 
 ## Installation directory for NetCDF static libraries
 ## (contains include and lib or lib64 subdirectories)
 # NETCDF4_DIR	= /usr
 # NETCDF4_DIR	= /usr/local
-NETCDF4_DIR	= /usr/local/netcdf_42
+NETCDF4_DIR	= /usr/local/netcdf_4211
 
 ## Java 1.6 jdk home directory - this may be predefined
 ## from your shell environment.  If JAVA_HOME is defined,

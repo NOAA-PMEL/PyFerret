29d20bb2d740fdc5736f476b72b481dca554ec9d
ksmith <ksmith@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2014-11-12 22:37:28 +0000

    Make default site_specific.mk files in pyferret configured for dunkel
    git-svn-id: file:///home/users/tmap/svn/repos/ferret/branches/pyferret@18733 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- external_functions/ef_utility/site_specific.mk
+++ external_functions/ef_utility/site_specific.mk
@@ -9,9 +9,9 @@
 ##   i386-linux-gnu    for 32-bit Ubuntu and many "free" Linux systems
 ## This value is used to determine which platform_specific.mk
 ## file to include in the Makefiles.
-# BUILDTYPE = $(HOSTTYPE)
+BUILDTYPE = $(HOSTTYPE)
 # BUILDTYPE = x86_64-linux
-BUILDTYPE = x86_64-linux-gnu
+# BUILDTYPE = x86_64-linux-gnu
 # BUILDTYPE = i386-linux
 # BUILDTYPE = i386-linux-gnu
 
@@ -26,8 +26,8 @@ BUILDTYPE = x86_64-linux-gnu
 INSTALL_FER_DIR = $(FER_DIR)
 
 ## Python version used by PyFerret, either python2.6 or python2.7
-# PYTHON_EXE = python2.6
-PYTHON_EXE = python2.7
+PYTHON_EXE = python2.6
+# PYTHON_EXE = python2.7
 
 ## FER_LOCAL_EXTFCNS is the directory in which to install
 ## the Ferret Fortran external functions.  The example
--- site_specific.mk
+++ site_specific.mk
@@ -3,7 +3,7 @@
 
 ## Full path name of the directory containing this file (the ferret root directory).
 ## Do not use $(shell pwd) since this is included in Makefiles in other directories.
-DIR_PREFIX	= $(HOME)/pyferret_dev
+DIR_PREFIX = $(HOME)/pyferret_dev
 
 ## Machine type for which to build Ferret/PyFerret
 ##   x86_64-linux      for 64-bit RHEL
@@ -11,64 +11,64 @@ DIR_PREFIX	= $(HOME)/pyferret_dev
 ##   i386-linux        for 32-bit RHEL
 ##   i386-linux-gnu    for 32-bit Ubuntu and many "free" Linux systems
 ##   i386-apple-darwin for MacOS
-BUILDTYPE	= $(HOSTTYPE)
-# BUILDTYPE	= x86_64-linux
-# BUILDTYPE	= x86_64-linux-gnu
-# BUILDTYPE	= i386-linux
-# BUILDTYPE	= i386-linux-gnu
-# BUILDTYPE	= i386-apple-darwin
-# BUILDTYPE	= intel-mac
+BUILDTYPE = $(HOSTTYPE)
+# BUILDTYPE = x86_64-linux
+# BUILDTYPE = x86_64-linux-gnu
+# BUILDTYPE = i386-linux
+# BUILDTYPE = i386-linux-gnu
+# BUILDTYPE = i386-apple-darwin
+# BUILDTYPE = intel-mac
 
 ## Python 2.x executable to invoke for build and install.
-PYTHON_EXE	= python2.6
-# PYTHON_EXE	= python2.7
+PYTHON_EXE = python2.6
+# PYTHON_EXE = python2.7
 ## The assignment of PYTHONINCDIR should not need any modifications
 PYTHONINCDIR   := $(shell $(PYTHON_EXE) -c "import distutils.sysconfig; print distutils.sysconfig.get_python_inc()")
 
 ## Installation directory for built Ferret.  Using the "install"
 ## Makefile target circumvents the need to create the fer_*.tar.gz
 ## files just for creating a Ferret installation.
-INSTALL_FER_DIR	= $(FER_DIR)
+INSTALL_FER_DIR = $(FER_DIR)
 
 ## Installation directory for Cairo-1.8.8 static libraries
 ## (contains include and lib or lib64 subdirectories) for RHEL5.
 ## Do not give a location on other systems.  For these systems
 ## the system-wide shared-object Cairo libraries which are also
 ## used by Qt4 must be used.
-CAIRO_DIR	=
-# CAIRO_DIR	= /usr/local/cairo_188
+# CAIRO_DIR =
+CAIRO_DIR = /usr/local/cairo_188
 
 ## Installation directory for HDF5 static libraries
 ## (contains include and lib or lib64 subdirectories)
 ## Do not give a location if linking to netcdf shared-object libraries
-# HDF5_DIR	= /usr
-# HDF5_DIR	= /usr/local
-# HDF5_DIR	= /usr/local/hdf5-1.8.9
-# HDF5_DIR	= /usr/local/hdf5_189
-HDF5_DIR	= /usr/local/hdf5_189_64
-# HDF5_DIR	= 
+# HDF5_DIR = /usr
+# HDF5_DIR = /usr/local
+# HDF5_DIR = /usr/local/hdf5-1.8.9
+HDF5_DIR = /usr/local/hdf5_189
+# HDF5_DIR = /usr/local/hdf5_189_64
+# HDF5_DIR = 
 
 ## Installation directory for NetCDF static or shared object libraries
 ## (contains include and lib or lib64 subdirectories)
 ## If HDF5_DIR (above) is empty, the shared-object netcdf libraries will be used.
-# NETCDF4_DIR	= /usr
-# NETCDF4_DIR	= /usr/local
-# NETCDF4_DIR	= /usr/local/netcdf-4.3.1.1
-# NETCDF4_DIR	= /usr/local/netcdf_4311
-NETCDF4_DIR	= /usr/local/netcdf_4311_64
+# NETCDF4_DIR = /usr
+# NETCDF4_DIR = /usr/local
+# NETCDF4_DIR = /usr/local/netcdf-4.3.1.1
+NETCDF4_DIR = /usr/local/netcdf_4311
+# NETCDF4_DIR = /usr/local/netcdf_4311_64
 
 ## Java home directory - this may be predefined
 ## from your shell environment.  If JAVA_HOME is defined,
 ## $(JAVA_HOME)/bin/javac and $(JAVA_HOME)/bin/jar is
 ## called to build threddsBrowser.jar; otherwise, it just
 ## uses javac and jar (from the path).
-# JAVA_HOME	= /usr/java/default
-# JAVA_HOME	= /usr/java/latest
-# JAVA_HOME	= /usr/lib/jvm/default-java
-# JAVA_HOME	= /usr/lib/jvm/java-oracle
-# JAVA_HOME	= /usr/lib/jvm/java-sun
-JAVA_HOME	= /usr/lib/jvm/java
-# JAVA_HOME	= /Library/Java/JavaVirtualMachines/jdk1.7.jdk/Contents/Home
+# JAVA_HOME = /usr/java/default
+# JAVA_HOME = /usr/java/latest
+# JAVA_HOME = /usr/lib/jvm/default-java
+# JAVA_HOME = /usr/lib/jvm/java-oracle
+# JAVA_HOME = /usr/lib/jvm/java-sun
+JAVA_HOME = /usr/lib/jvm/java
+# JAVA_HOME = /Library/Java/JavaVirtualMachines/jdk1.7.jdk/Contents/Home
 
 # PyFerret version number - do not change this
 PYFERRET_VERSION = 1.1.0

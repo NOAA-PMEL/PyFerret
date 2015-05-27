a574c0c52752c0c1b46d03c81ee226cbe1c6e60f
ansley <ansley@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2015-05-27 23:58:04 +0000

    restore mistaken checkin
    git-svn-id: file:///home/users/tmap/svn/repos/ferret/trunk@19662 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- site_specific.mk
+++ site_specific.mk
@@ -10,28 +10,25 @@ BUILDTYPE	= $(HOSTTYPE)
 ## Installation directory for built Ferret.  Using the "install"
 ## Makefile target circumvents the need to create the fer_*.tar.gz
 ## files just for creating a Ferret installation.
-#INSTALL_FER_DIR = $(FER_DIR)
-INSTALL_FER_DIR = /home/users/ansley/ferret_distributions/rhel6_64
+INSTALL_FER_DIR = /home/users/ansley/ferret_distributions/rhel5_64
 
 ## Installation directory for HDF5 static libraries
 ## (contains include and lib or lib64 subdirectories)
 # HDF5_DIR	= /usr
 # HDF5_DIR	= /usr/local
-HDF5_DIR	= /usr/local/hdf5_189_64
+HDF5_DIR	= /usr/local/hdf5_189
 
 ## Installation directory for NetCDF static libraries
 ## (contains include and lib or lib64 subdirectories)
 # NETCDF4_DIR	= /usr
 # NETCDF4_DIR	= /usr/local
-NETCDF4_DIR	= /usr/local/netcdf_432_64
-NETCDF4_DIR	= /home/users/ansley/local/flat_64_nc4331
+NETCDF4_DIR	= /usr/local/netcdf_432
 
 ## Installation directory for readline static libraries
 ## (contains include and lib or lib64 subdirectories)
 # READLINE_DIR	= /
 READLINE_DIR	= /usr
-#READLINE_DIR	= /usr/local
-#READLINE_DIR	= /home/users/tmap/flat_64/readline-4.1
+# READLINE_DIR	= /usr/local
 
 ## Installation directory for libz static library
 ## (contains include and lib or lib64 subdirectories)
@@ -40,14 +37,14 @@ READLINE_DIR	= /usr
 # LIBZ_DIR	= /usr
 LIBZ_DIR	= /usr/local
 
-## Java 1.6 jdk home directory - this may be predefined
+## Java home directory - this may be predefined
 ## from your shell environment.  If JAVA_HOME is defined,
 ## $(JAVA_HOME)/bin/javac and $(JAVA_HOME)/bin/jar is
 ## called to build threddsBrowser.jar; otherwise, it just
 ## uses javac and jar (from the path).
 # JAVA_HOME	= /usr/java/latest
-# JAVA_HOME	= /usr/lib/jvm/java-1.6.0-sun
-# JAVA_HOME	= /usr/lib/jvm/java-6-sun
-JAVA_HOME	= /usr/lib/jvm/java-sun
+# JAVA_HOME	= /usr/lib/jvm/java-oracle
+# JAVA_HOME	= /usr/lib/jvm/java-sun
+JAVA_HOME	= /usr/lib/jvm/java
 
 ##

74e95b97283fbbd4acf5d751530164ed56e9b602
ksmith <ksmith@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2016-03-16 18:52:20 +0000

    Update from Ferret trunk
    git-svn-id: file:///home/users/tmap/svn/repos/ferret/branches/pyferret@21459 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- bench/testferpyvar.py
+++ bench/testferpyvar.py
@@ -15,8 +15,8 @@ print ">>> datacopy = coads.sst._dataarray"
 datacopy = coads.sst._dataarray
 print ">>> datacopy[87:93,43:47,0,:,0,0] = -5.0"
 datacopy[87:93,43:47,0,:,0,0] = -5.0
-print ">>> coads.sstcopy = pyferret.FerPyVar(coads.sst._datagrid, datacopy, coads.sst._missingvalue, coads.sst._dataunit, 'modified copy of SST')"
-coads.sstcopy = pyferret.FerPyVar(coads.sst._datagrid, datacopy, coads.sst._missingvalue, coads.sst._dataunit, 'modified copy of SST')
+print ">>> coads.sstcopy = pyferret.FerPyVar(grid=coads.sst._datagrid, data=datacopy, missval=coads.sst._missingvalue, unit=coads.sst._dataunit, title='modified copy of SST')"
+coads.sstcopy = pyferret.FerPyVar(grid=coads.sst._datagrid, data=datacopy, missval=coads.sst._missingvalue, unit=coads.sst._dataunit, title='modified copy of SST')
 print ">>> pyferret.showdata(brief=False)"
 pyferret.showdata(brief=False)
 print ">>> pyferret.setwindow(1, axisasp=0.5, logo=False)"
@@ -27,6 +27,11 @@ print ">>> pyferret.shade(coads.sstcopy['40S':'40N','100E':'100W','16-FEB'])"
 pyferret.shade(coads.sstcopy['40S':'40N','100E':'100W','16-FEB'])
 print ">>> pyferret.saveplot('testferpyvar.pdf')"
 pyferret.saveplot('testferpyvar.pdf')
+
+# print ">>> pyferret.run('let sstdiff = sst - sstcopy')"
+# pyferret.run('let sstdiff = sst - sstcopy')
+# print ">>> pyferret.run('vtree sstdiff')"
+# pyferret.run('vtree sstdiff')
 
 print ">>> del coads.sstcopy"
 del coads.sstcopy

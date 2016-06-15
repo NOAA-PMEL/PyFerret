7271becfe9d8c6523be97cadb9d259cd6053dfd9
ksmith <ksmith@fdbf22ae-c210-0410-be80-ca943da6b8f8>
2016-06-15 23:59:33 +0000

    Change default configuration values in site_specific.mk files back to what they were (RHEL6-64)
    git-svn-id: file:///home/users/tmap/svn/repos/ferret/trunk@21933 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- site_specific.mk
+++ site_specific.mk
@@ -2,17 +2,17 @@
 
 ## Machine for which to build Ferret
 ## Use $(HOSTTYPE) to build natively for the machine you are using
-# BUILDTYPE	= $(HOSTTYPE)
+BUILDTYPE	= $(HOSTTYPE)
 # BUILDTYPE	= x86_64-linux
 # BUILDTYPE	= i386-linux
 # BUILDTYPE	= i386-apple-darwin
-BUILDTYPE	= intel-mac
+# BUILDTYPE	= intel-mac
 
 ## Installation directory for built Ferret.  Using the "install"
 ## Makefile target circumvents the need to create the fer_*.tar.gz
 ## files just for creating a Ferret installation.
-# INSTALL_FER_DIR = $(HOME)/ferret_distributions/rhel6_64
-INSTALL_FER_DIR = $(FER_DIR)
+INSTALL_FER_DIR = $(HOME)/ferret_distributions/rhel6_64
+# INSTALL_FER_DIR = $(FER_DIR)
 
 ## Installation directory for HDF5 static libraries
 ## (contains include and lib or lib64 subdirectories)
@@ -29,15 +29,15 @@ NETCDF4_DIR	= /usr/local/netcdf-4.4.0
 ## Installation directory for readline static libraries
 ## (contains include and lib or lib64 subdirectories)
 # READLINE_DIR	= /
-READLINE_DIR	= /usr
-# READLINE_DIR	= /usr/local
+# READLINE_DIR	= /usr
+READLINE_DIR	= /usr/local
 
 ## Installation directory for libz static library
 ## (contains include and lib or lib64 subdirectories)
 ## Version 1.2.5 or higher recommended by NetCDF
 # LIBZ_DIR	= /
-LIBZ_DIR	= /usr
-# LIBZ_DIR	= /usr/local
+# LIBZ_DIR	= /usr
+LIBZ_DIR	= /usr/local
 
 ## Java home directory - this may be predefined
 ## from your shell environment.  If JAVA_HOME is defined,
@@ -47,7 +47,7 @@ LIBZ_DIR	= /usr
 # JAVA_HOME	= /usr/java/latest
 # JAVA_HOME	= /usr/lib/jvm/java-oracle
 # JAVA_HOME	= /usr/lib/jvm/java-sun
-# JAVA_HOME	= /usr/lib/jvm/java
-JAVA_HOME	= /Library/Java/JavaVirtualMachines/jdk1.8.0_60.jdk/Contents/Home
+JAVA_HOME	= /usr/lib/jvm/java
+# JAVA_HOME	= /Library/Java/JavaVirtualMachines/jdk1.8.0_60.jdk/Contents/Home
 
 ##

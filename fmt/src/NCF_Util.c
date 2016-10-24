058d5d0b8dbf52903e2fc1f16410cde2772eb6d4
Ansley Manke <ansley.b.manke@noaa.gov>
2016-10-24 18:51:12 +0000

    Finished ticket 2422: increase max grids, max datasets
    git-svn-id: file:///home/users/tmap/svn/repos/ferret/trunk@22684 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- fmt/src/NCF_Util.c
+++ fmt/src/NCF_Util.c
@@ -190,6 +190,7 @@ int  FORTRAN(ncf_set_uvar_aux_info)( int *, int *, int *, int *, int *);
 int  FORTRAN(ncf_get_uvar_aux_info)( int *, int *, int *, int *, int *);
 int  FORTRAN(ncf_get_uvar_grid_list_len)( int *, int *, int *);
 int  FORTRAN(ncf_delete_uvar_grid)( int *, int *, int *);
+int  FORTRAN(ncf_next_uvar_grid_in_list)( int *, int *, int *, int *);
 
 /* .... Functions only called internally (thus static) .... */
 
@@ -2938,6 +2939,56 @@ int FORTRAN(ncf_free_uvar_grid_list)( int *LIST_dset, int *uvarid )
 
 /* ----
  * Find variable based on its variable ID and LIST_dset ID
+ * return the grid corresponding to the ith element in the grid list
+ */
+int FORTRAN(ncf_next_uvar_grid_in_list)( int *LIST_dset, int *uvarid, int *ith, int *grid )
+{
+    LIST *varlist;
+    int status;
+    ncvar *var_ptr;
+    LIST *uvgridList;
+    uvarGrid *uvgrid_ptr;
+	int count;
+	int i;
+
+    /*
+     * Get the list of variables, find pointer to variable varid.
+     */
+    varlist = ncf_get_ds_varlist(LIST_dset);
+    if ( varlist == NULL )
+        return ATOM_NOT_FOUND;
+
+    /* find the relevant LET var (i.e. uvar) */
+    status = list_traverse(varlist, (char *) uvarid, NCF_ListTraverse_FoundUvarID, (LIST_FRNT | LIST_FORW | LIST_ALTR));
+    if ( status != LIST_OK )
+        return ATOM_NOT_FOUND;
+    var_ptr = (ncvar *) list_curr(varlist);
+
+    /* remove all elements from the uvar grid list but do not remove the list itself */
+    uvgridList = var_ptr->uvarGridList;
+    if ( uvgridList != NULL ) {
+
+
+    /*
+     * Return the ith grid
+     */
+    list_mvfront(uvgridList);
+	count = 1;
+    for (i = 0; i < *ith; i++) {
+		uvgrid_ptr = (uvarGrid *) list_curr(uvgridList);
+        *grid = uvgrid_ptr->grid;
+		list_mvnext(uvgridList); 
+        } 
+
+    }
+
+    return FERR_OK;
+}
+
+
+
+/* ----
+ * Find variable based on its variable ID and LIST_dset ID
  * Store a grid/context_dset pair for the variable
  *
  * The dual dataset arguments arise because Ferret's global uvars are managed
@@ -2954,6 +3005,7 @@ int FORTRAN(ncf_set_uvar_grid)( int *LIST_dset, int *varid, int *grid, int *data
     LIST *uvgridlist;
     uvarGrid *uvgrid_ptr;
     uvarGrid uvgrid;
+	int uvgrid_list_len;
     int i;
 
     /*
@@ -2999,6 +3051,11 @@ int FORTRAN(ncf_set_uvar_grid)( int *LIST_dset, int *varid, int *grid, int *data
      * Save it in the grid list of this uvar
      */
     list_insert_after(uvgridlist, (char *) &uvgrid, sizeof(uvarGrid));
+	
+    uvgrid_list_len = (int) list_size(uvgridlist);
+	if (uvgrid_list_len > 1)
+	{ i = 1;
+	}
 
     return FERR_OK;
 }

d9d829b307640f25bd96f22d45524a862eed6e00
Karl Smith <karl.smith@noaa.gov>
2017-01-22 19:53:06 +0000

    Just some trivial rearranging and addition of comments and defines
    git-svn-id: file:///home/users/tmap/svn/repos/ferret/branches/pyferret@23140 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- external_functions/ef_utility/site_specific.mk
+++ external_functions/ef_utility/site_specific.mk
@@ -1,6 +1,7 @@
 ## Site-dependent definitions included in external function 
 ## Makefiles in an installed PyFerret directory.
 
+## =========================
 ## Machine type for which to build Ferret/PyFerret
 ##   x86_64-linux      for 64-bit RHEL
 ##   x86_64-linux-gnu  for 64-bit Ubuntu and many "free" Linux systems
@@ -9,6 +10,7 @@
 ##   intel-mac         for Max OSX
 ## This value is used to determine which platform_specific.mk
 ## file to include in the Makefiles.
+## =========================
 BUILDTYPE = $(HOSTTYPE)
 # BUILDTYPE = x86_64-linux
 # BUILDTYPE = x86_64-linux-gnu
@@ -16,25 +18,32 @@ BUILDTYPE = $(HOSTTYPE)
 # BUILDTYPE = i386-linux-gnu
 # BUILDTYPE = intel-mac
 
+## =========================
 ## INSTALL_FER_DIR and PYTHON_EXE are only used to construct
 ## the location of pyferret library.  The library should be
 ## (for either 32-bit or 64-bit Linux)
 ## $(INSTALL_FER_DIR)/lib/$(PYTHON_EXE)/site-package/pyferret/libpyferret.so
 ## or possibly (for 64-bit Linux only)
 ## $(INSTALL_FER_DIR)/lib64/$(PYTHON_EXE)/site-package/pyferret/libpyferret.so
-
+##
 ## PyFerret installation directory, usually just $(FER_DIR)
+## =========================
+# INSTALL_FER_DIR = $(HOME)/PyFerret
 INSTALL_FER_DIR = $(FER_DIR)
 
+## =========================
 ## Python version used by PyFerret
+## =========================
 PYTHON_EXE = python2.6
 # PYTHON_EXE = python2.7
 # PYTHON_EXE = python3.6
 
+## =========================
 ## FER_LOCAL_EXTFCNS is the directory in which to install
 ## the Ferret Fortran external functions.  The example
 ## functions that come with the PyFerret installation are
 ## installed in $(INSTALL_FER_DIR)/ext_func/pylibs
+## =========================
 FER_LOCAL_EXTFCNS = $(INSTALL_FER_DIR)/ext_func/pylibs
 
 ##
--- site_specific.mk
+++ site_specific.mk
@@ -1,18 +1,31 @@
 ## Site-dependent definitions included in Makefiles
 ## Also verify the values in external_functions/ef_utility/site_specific.mk
 
+## =========================
 ## Full path name of the directory containing this file (the ferret root directory).
 ## Do not use $(shell pwd) since this is included in Makefiles in other directories.
+## =========================
 DIR_PREFIX = $(HOME)/build/pyferret_dev
-# DIR_PREFIX = $(HOME)/pyferret_dev
 # DIR_PREFIX = $(HOME)/git/PyFerret
+# DIR_PREFIX = $(HOME)/pyferret_dev
 
+## =========================
+## Installation directory for built PyFerret.  Using the "install"
+## Makefile target automatically creates a generic pyferret-*.tar.gz
+## file and then extracts it to create a PyFerret installation.
+## =========================
+INSTALL_FER_DIR = $(HOME)/ferret_distributions/rhel6_64
+# INSTALL_FER_DIR = $(HOME)/PyFerret
+# INSTALL_FER_DIR = $(FER_DIR)
+
+## =========================
 ## Machine type for which to build Ferret/PyFerret
 ##   x86_64-linux      for 64-bit RHEL
 ##   x86_64-linux-gnu  for 64-bit Ubuntu and many "free" Linux systems
 ##   i386-linux        for 32-bit RHEL
 ##   i386-linux-gnu    for 32-bit Ubuntu and many "free" Linux systems
 ##   intel-mac         for Mac OSX
+## =========================
 BUILDTYPE = $(HOSTTYPE)
 # BUILDTYPE = x86_64-linux
 # BUILDTYPE = x86_64-linux-gnu
@@ -20,76 +33,87 @@ BUILDTYPE = $(HOSTTYPE)
 # BUILDTYPE = i386-linux-gnu
 # BUILDTYPE = intel-mac
 
+## =========================
 ## Python executable to invoke for build and install.
+## =========================
 PYTHON_EXE = python2.6
 # PYTHON_EXE = python2.7
 # PYTHON_EXE = python3.6
 
+## =========================
 ## The assignment of PYTHONINCDIR should not need any modifications
+## =========================
 PYTHONINCDIR := $(shell $(PYTHON_EXE) -c "from __future__ import print_function ; import distutils.sysconfig; print(distutils.sysconfig.get_python_inc())")
 
-## Installation directory for built Ferret.  Using the "install"
-## Makefile target circumvents the need to create the fer_*.tar.gz
-## files just for creating a Ferret installation.
-INSTALL_FER_DIR = $(HOME)/ferret_distributions/rhel6_64
-# INSTALL_FER_DIR = $(FER_DIR)
-# INSTALL_FER_DIR = $(HOME)/PyFerret
-
+## =========================
 ## Installation directory for cairo v1.12 or later static library
 ## (contains include and lib or lib64 subdirectories).  If blank,
 ## the system's cairo shared library will be used.  Older versions
 ## of cairo (v1.8 or later) can be used, but raster images from
 ## -nodisplay may look a little fuzzy unless -gif is specified.
+## =========================
 CAIRO_DIR = /usr/local/cairo-1.14.4
+# CAIRO_DIR = $(HOME)/.local
 # CAIRO_DIR = /usr/local
 # CAIRO_DIR =
-# CAIRO_DIR = $(HOME)/.local
 
+## =========================
 ## Installation directory for pixman-1 static library (contains
 ## include and lib or lib64 subdirectories) used by the above
 ## cairo library.  If blank, or if CAIRO_DIR is blank, the system's
 ## pixman-1 shared library will be used.
+## =========================
 PIXMAN_DIR = /usr/local/cairo-1.14.4
 # PIXMAN_DIR = /usr/local/pixman-1-0.34.0
+# PIXMAN_DIR = $(HOME)/.local
 # PIXMAN_DIR = /usr/local
 # PIXMAN_DIR =
-# PIXMAN_DIR = $(HOME)/.local
 
+## =========================
 ## Directory containing the pango-1.0, glib-2.0, freetype, fontconfig,
 ## png libraries (contains include and lib or lib64 subdirectory).
 ## If blank, or if CAIRO_DIR is blank, the system standard libraries
 ## will be used.
-PANGO_DIR =
-# PANGO_DIR = /usr/local
+## =========================
 # PANGO_DIR = $(HOME)/.local
+# PANGO_DIR = /usr/local
+PANGO_DIR =
 
+## =========================
 ## Installation directory for HDF5 static libraries (contains include
 ## and lib or lib64 subdirectories).  To link to NetCDF shared-object 
 ## libraries, do not give a location for HDF5_DIR.
+## =========================
 HDF5_DIR = /usr/local/hdf5-1.8.16
+# HDF5_DIR = $(HOME)/.local
 # HDF5_DIR = /usr/local
 # HDF5_DIR =
-# HDF5_DIR = $(HOME)/.local
 
+## =========================
 ## Compression library used in the above HDF5 library, either 'z'
 ## or 'sz'.  If HDF5_DIR is not given, this value is not used.
+## =========================
 COMPRESS_LIB = z
 # COMPRESS_LIB = sz
 
+## =========================
 ## Installation directory for NetCDF static or shared object libraries
 ## (contains include and lib or lib64 subdirectories).  If HDF5_DIR
 ## (above) is blank, the netcdf shared-object (.so) libraries will be
 ## used;  otherwise the netcdf static (.a) libraries will be used.
+## =========================
 NETCDF4_DIR = /usr/local/netcdf-4.4.0
-# NETCDF4_DIR = /usr/local
 # NETCDF4_DIR = $(HOME)/.local
+# NETCDF4_DIR = /usr/local
 
+## =========================
 ## Java home directory - this may be predefined
 ## from your shell environment.  If JAVA_HOME is defined,
 ## $(JAVA_HOME)/bin/javac and $(JAVA_HOME)/bin/jar is
 ## called to build threddsBrowser.jar; otherwise,
 ## threddsBrowser.jar is not built and the Ferret command
 ## SET DATA /BROWSE (or the alias OPEN) will not work.
+## =========================
 # JAVA_HOME = /usr/java/default
 # JAVA_HOME = /usr/java/latest
 # JAVA_HOME = /usr/lib/jvm/default-java
@@ -97,3 +121,4 @@ NETCDF4_DIR = /usr/local/netcdf-4.4.0
 JAVA_HOME = /usr/lib/jvm/java
 # JAVA_HOME = /Library/Java/JavaVirtualMachines/jdk1.8.0_112.jdk/Contents/Home
 
+##

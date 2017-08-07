2d08c1de8ef8060533dd0b8ceabc59c75b9afd40
Karl Smith <karl.smith@noaa.gov>
2017-08-07 23:20:59 +0000

    Update from ferret trunk - more thorough MEMORYDEBUG checking
    git-svn-id: file:///home/users/tmap/svn/repos/ferret/branches/pyferret@24086 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- fer/special/FerMem_routines.c
+++ fer/special/FerMem_routines.c
@@ -19,6 +19,7 @@
  */
 
 #ifdef MEMORYDEBUG
+
 #define DEBUGFILENAME "memorydebug.txt"
 static int initialized = 0;
 /*
@@ -26,7 +27,8 @@ static int initialized = 0;
  * If <endptr> is NULL, blanks are printed instead of the NULL address.
  * For consistency, use format "%016p" to print pointers in msg.
  */
-void FerMem_WriteDebugMessage(void *startptr, void *endptr, const char *msg) {
+void FerMem_WriteDebugMessage(void *startptr, void *endptr, const char *msg) 
+{
     FILE *debugfile;
     if ( ! initialized ) {
         initialized = 1;
@@ -47,6 +49,126 @@ void FerMem_WriteDebugMessage(void *startptr, void *endptr, const char *msg) {
     }
     fclose(debugfile);
 }
+
+#define FERMEM_BUFSIZE 8
+#define FERMEM_BUFVALUE 0xAAAAAAAA
+
+typedef struct _MemInfo_ {
+    size_t  size;
+    struct _MemInfo_ *next;
+    size_t *headbufptr;
+    size_t *tailbufptr;
+    void   *memory;
+    size_t  dummy[2*FERMEM_BUFSIZE];
+} MemInfo;
+
+static MemInfo *MemInfoList = NULL;
+
+/*
+ * Adds the given pointer to the linked list of memory pointers.
+ * Assumes the given pointer is allocated for 
+ *     'size' + sizeof(MemInfo)
+ * bytes of memory; the pointer to 'size' bytes of user memory 
+ * (the memory to be used as desired) is returned.
+ */
+static void *AddToMemInfoList(void *memplus, size_t size)
+{
+    MemInfo *memptr;
+    int      k;
+    void    *ptr;
+
+    if ( MemInfoList == NULL ) {
+        /* No list so make this memory the start of the list */
+        MemInfoList = (MemInfo *) memplus;
+    }
+    else {
+        /* Add this memory to the end of the list */
+        for (memptr = MemInfoList; ; memptr = memptr->next) {
+            /* First check the buffers are not corrupt */
+            for (k = 0; k < FERMEM_BUFSIZE; k++) {
+                if ( memptr->headbufptr[k] != FERMEM_BUFVALUE ) {
+                    fprintf(stderr, "Memory underwrite detected for allocation %016p of %ld bytes\n", memptr->memory, memptr->size);
+                    abort();
+                }
+            }
+            for (k = 0; k < FERMEM_BUFSIZE; k++) {
+                if ( memptr->tailbufptr[k] != FERMEM_BUFVALUE ) {
+                    fprintf(stderr, "Memory overwrite detected for allocation %016p of %ld bytes\n", memptr->memory, memptr->size);
+                    abort();
+                }
+            }
+            if ( memptr->next == NULL ) {
+                memptr->next = memplus;
+                break;
+            }
+        }
+    }
+
+    /* Initialize the MemInfo block */
+    memptr = (MemInfo *) memplus;
+    memptr->next = NULL;
+    memptr->size = size;
+    ptr = memptr;
+    ptr += sizeof(size_t);
+    ptr += sizeof(MemInfo *);
+    ptr += sizeof(size_t *);
+    ptr += sizeof(size_t *);
+    ptr += sizeof(void *);
+    memptr->headbufptr = (size_t *) ptr;
+    ptr += FERMEM_BUFSIZE * sizeof(size_t);
+    memptr->memory = ptr;
+    ptr += size;
+    memptr->tailbufptr = (size_t *) ptr;
+    for (k = 0; k < FERMEM_BUFSIZE; k++)
+        memptr->headbufptr[k] = FERMEM_BUFVALUE;
+    for (k = 0; k < FERMEM_BUFSIZE; k++)
+        memptr->tailbufptr[k] = FERMEM_BUFVALUE;
+    /* Return the pointer to part of the memory to be used */
+    return memptr->memory;
+}
+
+/*
+ * Removes the memory list entry associated with the given user memory pointer
+ * and returns the pointer to the original allocated memory.
+ * The size of the user memory that was allocated is returned as the value 
+ * pointed to by sizeptr.
+ */
+void *RemoveFromMemInfoList(void *ptr, size_t *sizeptr)
+{
+    MemInfo *prevptr;
+    MemInfo *memptr;
+    int      k;
+
+    for (prevptr = NULL, memptr = MemInfoList; memptr != NULL; prevptr = memptr, memptr = memptr->next) {
+        /* First check the buffers are not corrupt */
+        for (k = 0; k < FERMEM_BUFSIZE; k++) {
+            if ( memptr->headbufptr[k] != FERMEM_BUFVALUE ) {
+                fprintf(stderr, "Memory underwrite detected for allocation %016p of %ld bytes\n", memptr->memory, memptr->size);
+                abort();
+            }
+        }
+        for (k = 0; k < FERMEM_BUFSIZE; k++) {
+            if ( memptr->tailbufptr[k] != FERMEM_BUFVALUE ) {
+                fprintf(stderr, "Memory overwrite detected for allocation %016p of %ld bytes\n", memptr->memory, memptr->size);
+                abort();
+            }
+        }
+        if ( memptr->memory == ptr ) {
+            /* remove this MemInfo from the list */
+            if ( prevptr != NULL )
+                prevptr->next = memptr->next;
+            else
+                MemInfoList = memptr->next;
+            memptr->next = NULL;
+            /* assign the size of the user memory and return the pointer to the original memory */
+            *sizeptr = memptr->size;
+            return memptr;
+        }
+    }
+    fprintf(stderr, "Attempt to free unallocated memory %016p\n", memptr);
+    abort();
+}
+
 #endif
 
 /*
@@ -60,19 +182,28 @@ void FerMem_WriteDebugMessage(void *startptr, void *endptr, const char *msg) {
  * should be turned into: 
  *     myptr = FerMem_Malloc(mysize, __FILE__, __LINE__);
  */
-void *FerMem_Malloc(size_t size, char *filename, int linenumber) {
-    void *result = PyMem_Malloc(size);
+void *FerMem_Malloc(size_t size, char *filename, int linenumber) 
+{
+    void *ptr;
 
 #ifdef MEMORYDEBUG
     char msg[256];
 
+    /* allocate required memory as well as for the surrounding MemInfo block */
+    ptr = PyMem_Malloc(size + sizeof(MemInfo));
+    ptr = AddToMemInfoList(ptr, size);
     /* initialize to non-zero junk to catch uninitialized memory usage */
-    memset(result, 0x6B, size);
+    memset(ptr, 0x6B, size);
     sprintf(msg, "memory malloc allocated for %ld bytes : file %s : line %d", size, filename, linenumber);
-    FerMem_WriteDebugMessage(result, result + size, msg);
+    FerMem_WriteDebugMessage(ptr, ptr + size, msg);
+
+#else
+
+    ptr = PyMem_Malloc(size);
+
 #endif
 
-    return result;
+    return ptr;
 }
 
 /*
@@ -86,20 +217,32 @@ void *FerMem_Malloc(size_t size, char *filename, int linenumber) {
  * should be turned into: 
  *     newptr = FerMem_Realloc(myptr, newsize, __FILE__, __LINE__);
  */
-void *FerMem_Realloc(void *ptr, size_t size, char *filename, int linenumber) {
+void *FerMem_Realloc(void *ptr, size_t size, char *filename, int linenumber) 
+{
     void *newptr;
 
 #ifdef MEMORYDEBUG
-    char msg[256];
-    sprintf(msg, "memory to be realloc freed : file %s : line %d", filename, linenumber);
-    FerMem_WriteDebugMessage(ptr, NULL, msg);
-#endif
+    void  *origptr;
+    size_t oldsize;
+    char   msg[256];
+
+    origptr = RemoveFromMemInfoList(ptr, &oldsize);
+    sprintf(msg, "memory to be realloc freed for %ld bytes : file %s : line %d", oldsize, filename, linenumber);
+    FerMem_WriteDebugMessage(ptr, ptr + oldsize, msg);
+
+    newptr = PyMem_Realloc(origptr, size + sizeof(MemInfo));
+
+    newptr = AddToMemInfoList(newptr, size);
+    /* initialize new memory to non-zero junk to catch uninitialized memory usage */
+    if ( size > oldsize )
+       memset(newptr + oldsize, 0x6B, size - oldsize);
+    sprintf(msg, "memory realloc allocated for %ld bytes : file %s : line %d", size, filename, linenumber);
+    FerMem_WriteDebugMessage(newptr, newptr + size, msg);
+
+#else
 
     newptr = PyMem_Realloc(ptr, size);
 
-#ifdef MEMORYDEBUG
-    sprintf(msg, "memory realloc allocated for %ld bytes : file %s : line %d", size, filename, linenumber);
-    FerMem_WriteDebugMessage(newptr, newptr + size, msg);
 #endif
 
     return newptr;
@@ -116,14 +259,23 @@ void *FerMem_Realloc(void *ptr, size_t size, char *filename, int linenumber) {
  * should be turned into: 
  *     FerMem_Free(mymem, __FILE__, __LINE__);
  */
-void FerMem_Free(void *ptr, char *filename, int linenumber) {
-
+void FerMem_Free(void *ptr, char *filename, int linenumber) 
+{
 #ifdef MEMORYDEBUG
-    char msg[256];
-    sprintf(msg, "memory to be freed : file %s : line %d", filename, linenumber);
-    FerMem_WriteDebugMessage(ptr, NULL, msg);
-#endif
+    void  *origptr;
+    size_t size;
+    char   msg[256];
+
+    origptr = RemoveFromMemInfoList(ptr, &size);
+    sprintf(msg, "memory to be free freed for %ld bytes : file %s : line %d", size, filename, linenumber);
+    FerMem_WriteDebugMessage(ptr, ptr + size, msg);
+
+    PyMem_Free(origptr);
+
+#else
 
     PyMem_Free(ptr);
+
+#endif
 }
 

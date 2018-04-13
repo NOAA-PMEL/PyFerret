69f54af741b9b786a80a38d8093543e97e3690ca
Karl Smith <karl.smith@noaa.gov>
2018-04-13 23:46:25 +0000

    Add the copy_dylibs.sh script that copies the required Homebrew .dylib files to a dylibs subdirectory
    git-svn-id: file:///home/users/tmap/svn/repos/ferret/trunk@25026 fdbf22ae-c210-0410-be80-ca943da6b8f8

---
+++ copy_dylibs.sh
@@ -0,0 +1,28 @@
+#! /bin/sh
+# Copies the Homebrew .dylibs required to run the ferret executable to a
+# dylibs subdirectory (creating it if not present) of the current directory.
+
+if [ ! -d dylibs ]; then
+    if [ -e dylibs ]; then
+        echo "dylibs exists but is not a directory"
+        exit 1
+    fi
+    mkdir dylibs
+fi
+cd dylibs
+
+brewprefix=`brew config | awk '/HOMEBREW_PREFIX/ {print $2}'`
+
+libdir="${brewprefix}/lib"
+echo "Copying dylib libraries from ${libdir}"
+for name in netcdff netcdf hdf5_hl hdf5 sz ; do
+    echo "    ${name}"
+    cp -f ${libdir}/lib${name}.*.dylib .
+done
+
+libdir=`echo ${brewprefix}/opt/gcc/lib/gcc/*`
+echo "Copying dylib libraries from ${libdir}"
+for name in gfortran quadmath gcc_s ; do
+    echo "    ${name}"
+    cp -f ${libdir}/lib${name}.*.dylib .
+done

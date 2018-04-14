8d89e094695be90650642c93789d6cbd8f2ad555
Karl Smith <karl.smith@noaa.gov>
2018-04-14 04:21:32 +0000

    Add SZ_LIBDIR to link in libsz if needed; bug fix on str_mask; remove most of the dylibs from copy_dylibs.sh as static linking netcdf, hdf5, and sz removes the need for those libraries as well as the libgfortran (which was needed for external functions)
    git-svn-id: file:///home/users/tmap/svn/repos/ferret/trunk@25028 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- copy_dylibs.sh
+++ copy_dylibs.sh
@@ -13,16 +13,9 @@ cd dylibs
 
 brewprefix=`brew config | awk '/HOMEBREW_PREFIX/ {print $2}'`
 
-libdir="${brewprefix}/lib"
-echo "Copying dylib libraries from ${libdir}"
-for name in netcdff netcdf hdf5_hl hdf5 sz ; do
-    echo "    ${name}"
-    cp -f ${libdir}/lib${name}.*.dylib .
-done
-
 libdir=`echo ${brewprefix}/opt/gcc/lib/gcc/*`
 echo "Copying dylib libraries from ${libdir}"
-for name in gfortran quadmath gcc_s ; do
+for name in quadmath gcc_s ; do
     echo "    ${name}"
     cp -f ${libdir}/lib${name}.*.dylib .
 done

561b770d3cfc5b44243db71077b768d1a06f7f22
Karl Smith <karl.smith@noaa.gov>
2018-06-14 21:39:39 +0000

    Update from ferret/trunk (issue 1868: transforms on scalar constant variables, change FMRC to sparse views requiring regular time/forecast axes); update bench/test_results
    git-svn-id: file:///home/users/tmap/svn/repos/ferret/branches/pyferret@25238 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- fer/ccr/cache_full_array.c
+++ fer/ccr/cache_full_array.c
@@ -57,7 +57,6 @@ void FORTRAN(cache_full_array)( double *array, int *alen, double **pointer_val)
 /*
   input  - array and alen (array length)
   output - pointer to array
-      note the "lie" here": FORTRAN actually passes the 
 */
 {
   double *ptr;

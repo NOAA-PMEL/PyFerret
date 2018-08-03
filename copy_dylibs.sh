6b939db841b04e6b02da4f37978736650753577e
Karl Smith <karl.smith@noaa.gov>
2018-08-03 15:49:18 +0000

    For intel-mac, do not try to copy homebrew libraries if brew command not found
    git-svn-id: file:///home/users/tmap/svn/repos/ferret/branches/pyferret@25422 fdbf22ae-c210-0410-be80-ca943da6b8f8

--- copy_dylibs.sh
+++ copy_dylibs.sh
@@ -12,28 +12,30 @@ fi
 cd dylibs
 
 brewprefix=`brew config | awk '/HOMEBREW_PREFIX/ {print $2}'`
+if [ -n "$brewprefix" ]; then
 
-libdir="${brewprefix}/lib"
-echo "Copying dylib libraries from ${libdir}"
-for name in cairo fontconfig freetype fribidi glib-2.0 gobject-2.0 graphite2 \
-            gthread-2.0 harfbuzz hdf5 hdf5_hl netcdf netcdff pango-1.0 \
-            pangocairo-1.0 pangoft2-1.0 pcre pixman-1 png16 sz ; do
-    echo "    ${name}"
-    cp -f ${libdir}/lib${name}.*.dylib .
-done
+    libdir="${brewprefix}/lib"
+    echo "Copying dylib libraries from ${libdir}"
+    for name in cairo fontconfig freetype fribidi glib-2.0 gobject-2.0 graphite2 \
+                gthread-2.0 harfbuzz hdf5 hdf5_hl netcdf netcdff pango-1.0 \
+                pangocairo-1.0 pangoft2-1.0 pcre pixman-1 png16 sz ; do
+        echo "    ${name}"
+        cp -f ${libdir}/lib${name}.*.dylib .
+    done
 
-libdir="${brewprefix}/Cellar/libffi/*/lib"
-echo "Copying ffi library from ${libdir}"
-cp -f ${libdir}/libffi.*.dylib .
+    libdir="${brewprefix}/Cellar/libffi/*/lib"
+    echo "Copying ffi library from ${libdir}"
+    cp -f ${libdir}/libffi.*.dylib .
 
-libdir="${brewprefix}/Cellar/gettext/*/lib"
-echo "Copying intl library from ${libdir}"
-cp -f ${libdir}/libintl.*.dylib .
+    libdir="${brewprefix}/Cellar/gettext/*/lib"
+    echo "Copying intl library from ${libdir}"
+    cp -f ${libdir}/libintl.*.dylib .
 
-libdir=`echo ${brewprefix}/opt/gcc/lib/gcc/*`
-echo "Copying dylib libraries from ${libdir}"
-for name in gfortran quadmath gcc_s ; do
-    echo "    ${name}"
-    cp -f ${libdir}/lib${name}.*.dylib .
-done
+    libdir=`echo ${brewprefix}/opt/gcc/lib/gcc/*`
+    echo "Copying dylib libraries from ${libdir}"
+    for name in gfortran quadmath gcc_s ; do
+        echo "    ${name}"
+        cp -f ${libdir}/lib${name}.*.dylib .
+    done
 
+fi
